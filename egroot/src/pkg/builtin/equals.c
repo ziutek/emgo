1c7c3a64426ae25f6cbf9c489c21530240523cfc
Michał Derkacz <michal@Lnet.pl>
2014-08-25 01:52:29 +0200

    String equality.

---
+++ egroot/src/pkg/builtin/equals.c
@@ -0,0 +1,14 @@
+#include <types/types.h>
+
+bool equals(string s1, string s2) {
+	if (s1.len != s2.len) {
+		return false;
+	}
+	int i;
+	for (i = 0; i < s1.len; ++i) {
+		if (s1.str[i] != s2.str[i]) {
+			return false;
+		}
+	}
+	return true;
+}
