e0465819dcf46e2b1f0dbea8f0118fd77636e555
Michał Derkacz <michal@Lnet.pl>
2014-09-11 14:09:42 +0200

    stack: all functions should have always_inline attribute.

--- egroot/src/pkg/stack/stack+.h
+++ egroot/src/pkg/stack/stack+.h
@@ -1,47 +1,47 @@
 __attribute__ ((always_inline))
 extern inline
-unsafe$Pointer stack$alloc(int n, uintptr size) {
+unsafe$Pointer stack$Alloc(int n, uintptr size) {
 	unsafe$Pointer p = __builtin_alloca(size);
 	__builtin_memset(p, 0, size);
 	return p;
 }
 
-#define _DEFFUNC(typ)                                      \
+#define _DEFFUNC(name, typ)                                \
 	__attribute__ ((always_inline))                        \
 	extern inline                                          \
-	slice stack$##typ##s(int n) {                            \
-		return (slice){stack$alloc(n, sizeof(typ)), n, n}; \
+	slice stack$##name(int n) {                            \
+		return (slice){stack$Alloc(n, sizeof(typ)), n, n}; \
 	}
 	
-_DEFFUNC(int)
-_DEFFUNC(uint)
-_DEFFUNC(uintptr)
-_DEFFUNC(bool)
-_DEFFUNC(interface)
+_DEFFUNC(Ints, int)
+_DEFFUNC(Uints, uint)
+_DEFFUNC(Uintptrs, uintptr)
+_DEFFUNC(Bools, bool)
+_DEFFUNC(Interfaces, interface)
 
 #undef _DEFFUNC
 
-#define _DEFFUNC(typ, bits)                           \
+#define _DEFFUNC(name, bits)                          \
 	__attribute__ ((always_inline))                   \
 	extern inline                                     \
-	slice stack$##typ##s##bits(int n) {               \
-		return (slice){stack$alloc(n, bits/8), n, n}; \
-	}                                                 \
+	slice stack$##name##bits(int n) {                 \
+		return (slice){stack$Alloc(n, bits/8), n, n}; \
+	}
 
-_DEFFUNC(int, 8)
-_DEFFUNC(int, 16)
-_DEFFUNC(int, 32)
-_DEFFUNC(int, 64)
+_DEFFUNC(Ints, 8)
+_DEFFUNC(Ints, 16)
+_DEFFUNC(Ints, 32)
+_DEFFUNC(Ints, 64)
 
-_DEFFUNC(uint, 8)
-_DEFFUNC(uint, 16)
-_DEFFUNC(uint, 32)
-_DEFFUNC(uint, 64)
+_DEFFUNC(Uints, 8)
+_DEFFUNC(Uints, 16)
+_DEFFUNC(Uints, 32)
+_DEFFUNC(Uints, 64)
 
-_DEFFUNC(float, 32)
-_DEFFUNC(float, 64)
+_DEFFUNC(Floats, 32)
+_DEFFUNC(Floats, 64)
 
-_DEFFUNC(complex, 64)
-_DEFFUNC(complex, 128)
+_DEFFUNC(Complexs, 64)
+_DEFFUNC(Complexs, 128)
 
 #undef _DEFFUNC
