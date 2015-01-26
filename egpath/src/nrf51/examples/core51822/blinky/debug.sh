0c0fa47d685aafcc10210a7de280599001648880
Michał Derkacz <michal@Lnet.pl>
2015-01-26 15:21:38 +0100

    First attempt to debug nRF51822

--- egpath/src/nrf51/examples/core51822/blinky/debug.sh
+++ egpath/src/nrf51/examples/core51822/blinky/debug.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-setsid st-util >/dev/null 2>&1 </dev/null &
+setsid openocd -f oocd-stlink.cfg >/dev/null 2>&1 </dev/null &
 
 trap /bin/true INT
 
 arm-none-eabi-gdb --tui \
-	-ex "target extended-remote localhost:4242" \
-	-ex "set remote hardware-breakpoint-limit 3" \
+	-ex "target extended-remote localhost:3333" \
+	-ex "set remote hardware-breakpoint-limit 4" \
 	-ex "set remote hardware-watchpoint-limit 2" \
 	main.elf
 
-killall st-util
+killall openocd
--- egpath/src/nrf51/examples/core51822/blinky/load.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-arm-none-eabi-objcopy -Obinary main.elf main.bin
-addr=0x20000000
-if [ $# -eq 1 -a "$1" = 'flash' ]; then
-	addr=0x8000000
-fi       
-echo "Loading at $addr..."
-st-flash --reset write main.bin $addr
