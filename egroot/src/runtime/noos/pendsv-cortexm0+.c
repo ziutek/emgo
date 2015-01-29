cdc1d365643eadff020e57ddda822512ffb4645e
Michał Derkacz <michal@Lnet.pl>
2015-01-29 14:56:39 +0100

    runtime/noos: PendSV handler: new for Cortex-M0, simpler for Cortex-M4F.

--- egroot/src/runtime/noos/pendsv-cortexm0+.c
+++ egroot/src/runtime/noos/pendsv-cortexm0+.c
@@ -16,18 +16,23 @@ void runtime$noos$pendSVHandler() {
 		
 		// Save the second part of the context of the current task.
 		"mrs	r1, psp\n\t"
-		"subs	r1, 8*4\n\t"
-		"stm	r1!, {r4-r7}\n\t"
+		"sub	r1, #8*4\n\t"
+		"stmia	r1!, {r4-r7}\n\t"
 		"mov    r4, r8\n\t"
 		"mov    r5, r9\n\t"
 		"mov    r6, r10\n\t"
 		"mov    r7, r11\n\t"
-		"stm	r1, {r4-r7}\n\t"	
+		"stmia	r1!, {r4-r7}\n\t"	
 		
 		// Restore the second part of the context of the next task.
 		"msr	psp, r0\n\t"
-		"subs	r0, 8*4\n\t" 
-		"ldm	r0, {r4-r11}\n\t"
+		"sub	r0, #8*4\n\t" 
+		"ldmia	r0!, {r4-r7}\n\t"
+		"mov    r8, r4\n\t"
+		"mov    r9, r5\n\t"
+		"mov    r10, r6\n\t"
+		"mov    r11, r7\n\t"
+		"ldmia	r0!, {r4-r7}\n\t"	
 		
 		"1:\n\t"
 	
--- egroot/src/runtime/noos/pendsv-cortexm4f+.c
+++ egroot/src/runtime/noos/pendsv-cortexm4f+.c
@@ -1,15 +1,15 @@
 // +build cortexm4f
 
-__attribute__ ((naked)) static
+/*__attribute__ ((naked)) static
 void runtime$noos$pendSVHandler() {
 	asm volatile (
 		"push	{lr}\n\t"
 		"mrs	r0, psp\n\t"
 		
 		// Set FPU bit in SP.
-		"tst	lr, 0x10\n\t"
+		"tst	lr, #0x10\n\t"
 		"it		eq\n\t"
-		"addeq	r0, 1\n\t"
+		"addeq	r0, #1\n\t"
 		
 		// Call nextTask with SP used by current task.
 		"bl		runtime$noos$nextTask\n\t"
@@ -24,7 +24,7 @@ void runtime$noos$pendSVHandler() {
 		"mrs	r1, psp\n\t"
 		"stmdb	r1!, {r4-r11}\n\t"
 		
-		"tst	r2, 0x10\n\t"
+		"tst	r2, #0x10\n\t"
 		"bne	0f\n\t"
 		
 		// Save the second part of the FPU context of the current task.
@@ -33,13 +33,13 @@ void runtime$noos$pendSVHandler() {
 		"0:\n\t"
 		
 		// Adjust new SP and EXC_RETURN according to the FPU bit in taskInfo.sp.
-		"tst	r0, 1\n\t"
+		"tst	r0, #1\n\t"
 		"ittt	eq\n\t"
-		"subeq	r0, 8*4\n\t"
-		"orreq	r2, 0x10\n\t"
+		"subeq	r0, #8*4\n\t"
+		"orreq	r2, #0x10\n\t"
 		"beq	0f\n\t"
-		"subs	r0, 1+(8+16)*4\n\t"
-		"bic	r2, 0x10\n\t"
+		"subs	r0, #1+(8+16)*4\n\t"
+		"bic	r2, #0x10\n\t"
 		
 		// Restore the second part of the FPU context of the next task.
 		"vldmia r0!, {s16-s31}\n\t"
@@ -52,6 +52,63 @@ void runtime$noos$pendSVHandler() {
 		
 		"1:\n\t"
 		
+		"bx		r2"
+		:: "X" (runtime$noos$nextTask)
+	);
+}*/
+
+__attribute__ ((naked)) static
+void runtime$noos$pendSVHandler() {
+	asm volatile (
+		"push	{lr}\n\t"
+		"mrs	r0, psp\n\t"
+		
+		// Set FPU bit in SP.
+		"tst	lr, #0x10\n\t"
+		"it		eq\n\t"
+		"addeq	r0, #1\n\t"
+		
+		// Call nextTask with SP used by current task.
+		"bl		runtime$noos$nextTask\n\t"
+		
+		"pop	{r2}\n\t"
+		
+		// Check whether the context switch is need (r0 contains taskInfo.sp
+		// for next task or 0 if context switch isn't need).
+		"cbz	r0, 1f\n\t"
+		
+		// Save the second part of the CPU context of the current task.
+		"mrs	r1, psp\n\t"
+		"stmdb	r1!, {r4-r11}\n\t" // Don't put this in IT block!
+		
+		"tst	r2, #0x10\n\t"
+		"bne	0f\n\t"
+		
+		// Save the second part of the FPU context of the current task.
+		"vstmdb	r1!, {s16-s31}\n\t" // Don't put this in IT block!
+		
+		"0:\n\t"
+		
+		// Adjust new SP and EXC_RETURN according to the FPU bit in taskInfo.sp.
+		"tst	r0, #1\n\t"
+		"itte	ne\n\t"
+		"subne	r0, #1\n\t"
+		"bicne	r2, #0x10\n\t"
+		"orreq	r2, #0x10\n\t"
+		
+		"msr	psp, r0\n\t"
+		
+		// Restore the second part of the CPU context of the next task.
+		"ldmdb	r0!, {r4-r11}\n\t" // Don't put this in IT block!
+		
+		"tst	r2, #0x10\n\t"
+		"bne	1f\n\t"
+		
+		// Restore the second part of the FPU context of the next task.
+		"vldmdb r0!, {s16-s31}\n\t" // Don't put this in IT block!
+		
+		"1:\n\t"
+					
 		"bx		r2"
 		:: "X" (runtime$noos$nextTask)
 	);
