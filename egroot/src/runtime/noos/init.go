587259e930890ec6d068c36923a49cfccd406011
Michał Derkacz <michal@Lnet.pl>
2015-02-25 20:25:11 +0100

    runtime/noos: Non-blocking allocator.

--- egroot/src/runtime/noos/init.go
+++ egroot/src/runtime/noos/init.go
@@ -1,7 +1,12 @@
 package noos
 
+import "builtin"
+
 func init() {
 	initCPU()
+	builtin.Alloc = alloc
+	builtin.MakeChan = makeChan
+	builtin.Select = selectComm
 	if MaxTasks() > 0 {
 		tasker.init()
 	}
