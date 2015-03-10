36ceed4af2f972255a57c49f30e105e27f17d910
Michał Derkacz <michal@Lnet.pl>
2015-03-10 14:01:34 +0100

    rtos: New Debug.WriteByte method.

--- egroot/src/rtos/debug.go
+++ egroot/src/rtos/debug.go
@@ -11,6 +11,11 @@ func (d Debug) Write(b []byte) (int, error) {
 	return syscall.DebugOut(int(d), b)
 }
 
+func (d Debug) WriteByte(b byte) error {
+	_, err := syscall.DebugOut(int(d), []byte{b})
+	return err
+}
+
 func (d Debug) WriteString(s string) (int, error) {
 	return syscall.DebugOutString(int(d), s)
 }
