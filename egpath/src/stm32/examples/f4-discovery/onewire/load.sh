d1bdb9cb8d81882635423b60bef81a5fe9405059
Michał Derkacz <michal@Lnet.pl>
2015-10-18 12:59:13 +0200

    examples: Update broken symlinks

--- egpath/src/stm32/examples/f4-discovery/onewire/load.sh
+++ egpath/src/stm32/examples/f4-discovery/onewire/load.sh
@@ -1,9 +1,1 @@
-#!/bin/sh
-
-arm-none-eabi-objcopy -O binary -R .noload main.elf main.bin
-addr=0x20000000
-if [ $# -eq 1 -a "$1" = 'flash' ]; then
-	addr=0x8000000
-fi       
-echo "Loading at $addr..."
-st-flash --reset write main.bin $addr
+../../utils/load.sh
