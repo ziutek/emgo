38295e610ad897923edaaeecbff887eb74a3ffb4
Michał Derkacz <michal@Lnet.pl>
2015-11-19 00:13:47 +0100

    cortexm: Remove always inline attribute from intrinsincs. systick: Minor changes.

--- egroot/src/arch/cortexm/intrinsics+.h
+++ egroot/src/arch/cortexm/intrinsics+.h
@@ -1,160 +1,132 @@
-__attribute__ ((always_inline))
-extern inline
-uint32 arch$cortexm$APSR() {
+uint32
+arch$cortexm$APSR() {
 	uintptr r;
-	asm volatile ("mrs %0, apsr" : "=r" (r));
+	asm volatile ("mrs %0, apsr":"=r" (r));
 	return r;
 }
 
-__attribute__ ((always_inline))
-extern inline
-void arch$cortexm$SetAPSR(uint32 r) {
-	asm volatile ("msr apsr, %0" :: "r" (r) : "apsr");
+void
+arch$cortexm$SetAPSR(uint32 r) {
+	asm volatile ("msr apsr, %0"::"r" (r):"apsr");
 }
 
-__attribute__ ((always_inline))
-extern inline
-uint32 arch$cortexm$IPSR() {
+uint32
+arch$cortexm$IPSR() {
 	uint32 r;
-	asm volatile ("mrs %0, ipsr" : "=r" (r));
+	asm volatile ("mrs %0, ipsr":"=r" (r));
 	return r;
 }
 
-__attribute__ ((always_inline))
-extern inline
-void arch$cortexm$SetIPSR(uint32 r) {
-	asm volatile ("msr ipsr, %0" :: "r" (r) : "ipsr");
+void
+arch$cortexm$SetIPSR(uint32 r) {
+	asm volatile ("msr ipsr, %0"::"r" (r):"ipsr");
 }
 
-__attribute__ ((always_inline))
-extern inline
-uint32 arch$cortexm$EPSR() {
+uint32
+arch$cortexm$EPSR() {
 	uint32 r;
-	asm volatile ("mrs %0, epsr" : "=r" (r));
+	asm volatile ("mrs %0, epsr":"=r" (r));
 	return r;
 }
 
-__attribute__ ((always_inline))
-extern inline
-uint32 cortexm$IEPSR() {
+uint32
+cortexm$IEPSR() {
 	uint32 r;
-	asm volatile ("mrs %0, iepsr" : "=r" (r));
+	asm volatile ("mrs %0, iepsr":"=r" (r));
 	return r;
 }
 
-__attribute__ ((always_inline))
-extern inline
-void arch$cortexm$SetIEPSR(uint32 r) {
-	asm volatile ("msr iepsr, %0" :: "r" (r) : "iepsr");
-}
-
-__attribute__ ((always_inline))
-extern inline
-uint32 arch$cortexm$IAPSR() {
+uint32
+arch$cortexm$IAPSR() {
 	uint32 r;
-	asm volatile ("mrs %0, iapsr" : "=r" (r));
+	asm volatile ("mrs %0, iapsr":"=r" (r));
 	return r;
 }
 
-__attribute__ ((always_inline))
-extern inline
-void arch$cortexm$SetIAPSR(uint32 r) {
-	asm volatile ("msr iapsr, %0" :: "r" (r) : "apsr");
+void
+arch$cortexm$SetIAPSR(uint32 r) {
+	asm volatile ("msr iapsr, %0"::"r" (r):"apsr");
 }
 
-__attribute__ ((always_inline))
-extern inline
-uint32 arch$cortexm$EAPSR() {
+uint32
+arch$cortexm$EAPSR() {
 	uint32 r;
-	asm volatile ("mrs %0, eapsr" : "=r" (r));
+	asm volatile ("mrs %0, eapsr":"=r" (r));
 	return r;
 }
 
-__attribute__ ((always_inline))
-extern inline
-void arch$cortexm$SetEAPSR(uint32 r) {
-	asm volatile ("msr eapsr, %0" :: "r" (r) : "eapsr");
+void
+arch$cortexm$SetEAPSR(uint32 r) {
+	asm volatile ("msr eapsr, %0"::"r" (r):"eapsr");
 }
 
-__attribute__ ((always_inline))
-extern inline
-uint32 arch$cortexm$PSR() {
+uint32
+arch$cortexm$PSR() {
 	uint32 r;
-	asm volatile ("mrs %0, psr" : "=r" (r));
+	asm volatile ("mrs %0, psr":"=r" (r));
 	return r;
 }
 
-__attribute__ ((always_inline))
-extern inline
-void arch$cortexm$SetPSR(uint32 r) {
-	asm volatile ("msr psr, %0" :: "r" (r) : "psr");
+void
+arch$cortexm$SetPSR(uint32 r) {
+	asm volatile ("msr psr, %0"::"r" (r):"psr");
 }
 
-__attribute__ ((always_inline))
-extern inline
-uintptr arch$cortexm$MSP() {
+uintptr
+arch$cortexm$MSP() {
 	uintptr p;
-	asm volatile ("mrs %0, msp" : "=r" (p));
+	asm volatile ("mrs %0, msp":"=r" (p));
 	return p;
 }
 
-__attribute__ ((always_inline))
-extern inline
-void arch$cortexm$SetMSP(unsafe$Pointer p) {
-	asm volatile ("msr msp, %0" :: "r" (p) : "sp");
+void
+arch$cortexm$SetMSP(unsafe$Pointer p) {
+	asm volatile ("msr msp, %0"::"r" (p):"sp");
 }
 
-__attribute__ ((always_inline))
-extern inline
-uintptr arch$cortexm$PSP() {
+uintptr
+arch$cortexm$PSP() {
 	uintptr p;
-	asm volatile ("mrs %0, psp" : "=r" (p));
+	asm volatile ("mrs %0, psp":"=r" (p));
 	return p;
 }
 
-__attribute__ ((always_inline))
-extern inline
-void arch$cortexm$SetPSP(unsafe$Pointer p) {
-	asm volatile ("msr psp, %0" :: "r" (p) : "sp");
+void
+arch$cortexm$SetPSP(unsafe$Pointer p) {
+	asm volatile ("msr psp, %0"::"r" (p):"sp");
 }
 
-__attribute__ ((always_inline))
-extern inline
-uint32 arch$cortexm$LR() {
+uint32
+arch$cortexm$LR() {
 	uint32 r;
-	asm volatile ("mov %0, lr" : "=r" (r));
+	asm volatile ("mov %0, lr":"=r" (r));
 	return r;
 }
 
-__attribute__ ((always_inline))
-extern inline
-void arch$ortexm$SetLR(uint32 r) {
-	asm volatile ("mov lr, %0" :: "r" (r) : "lr");
+void
+arch$ortexm$SetLR(uint32 r) {
+	asm volatile ("mov lr, %0"::"r" (r):"lr");
 }
 
-__attribute__ ((always_inline))
-extern inline
-arch$cortexm$Control arch$cortexm$Ctrl() {
+arch$cortexm$Control
+arch$cortexm$Ctrl() {
 	arch$cortexm$Control c;
-	asm volatile ("mrs %0, control" : "=r" (c));
+	asm volatile ("mrs %0, control":"=r" (c));
 	return c;
 }
 
-__attribute__ ((always_inline))
-extern inline
-void arch$cortexm$SetCtrl(arch$cortexm$Control c) {
-	asm volatile ("msr control, %0" :: "r" (c));
+void
+arch$cortexm$SetCtrl(arch$cortexm$Control c) {
+	asm volatile ("msr control, %0"::"r" (c));
 }
 
-__attribute__ ((always_inline))
-extern inline
-void arch$cortexm$SEV() {
+void
+arch$cortexm$SEV() {
 	asm volatile ("sev");
 }
 
-__attribute__ ((always_inline))
-extern inline
-void arch$cortexm$ISB() {
+void
+arch$cortexm$ISB() {
 	asm volatile ("isb");
 }
 
--- egroot/src/arch/cortexm/intrinsics.go
+++ egroot/src/arch/cortexm/intrinsics.go
@@ -2,42 +2,61 @@ package cortexm
 
 import "unsafe"
 
+//c:static inline
 func APSR() uint32
 
+//c:static inline
 func SetAPSR(r uint32)
 
+//c:static inline
 func IPSR() uint32
 
+//c:static inline
 func SetIPSR(r uint32)
 
+//c:static inline
 func EPSR() uint32
 
+//c:static inline
 func SetEPSR(r uint32)
 
+//c:static inline
 func IEPSR() uint32
 
+//c:static inline
 func IAPSR() uint32
 
+//c:static inline
 func SetIAPSR(r uint32)
 
+//c:static inline
 func EAPSR() uint32
 
+//c:static inline
 func SetEAPSR(r uint32)
 
+//c:static inline
 func PSR() uint32
 
+//c:static inline
 func SetPSR(r uint32)
 
+//c:static inline
 func MSP() uintptr
 
+//c:static inline
 func SetMSP(p unsafe.Pointer)
 
+//c:static inline
 func PSP() uintptr
 
+//c:static inline
 func SetPSP(p unsafe.Pointer)
 
+//c:static inline
 func LR() uint32
 
+//c:static inline
 func SetLR(r uint32)
 
 type Control uint32
@@ -48,14 +67,18 @@ const (
 	FPCA                       // Floating-point context active
 )
 
+//c:static inline
 func SetCtrl(c Control)
 
+//c:static inline
 func Ctrl() Control
 
+//c:static inline
 func SEV()
 
-func SVC(imm byte)
-
+//c:static inline
 func ISB()
+
+func SVC(imm byte)
 
 func BKPT(imm byte)
