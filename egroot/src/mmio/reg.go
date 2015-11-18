e6ffb37b83c822e5c8cec2a97b8a7fe6ee8076db
Michał Derkacz <michal@Lnet.pl>
2015-11-18 14:02:29 +0100

    mmio: Names hanged: Reg -> U

--- egroot/src/mmio/reg.go
+++ egroot/src/mmio/reg.go
@@ -2,110 +2,158 @@ package mmio
 
 import "unsafe"
 
-type Reg8 struct {
+type U8 struct {
 	r uint8
 } //c:volatile
 
-func PtrReg8(addr uintptr) *Reg8 {
-	return (*Reg8)(unsafe.Pointer(addr))
+func PtrU8(addr unsafe.Pointer) *U8 {
+	return (*U8)(addr)
 }
 
-func (r *Reg8) SetBit(n int) {
-	r.r |= uint8(1) << uint(n)
+func AsU8(addr *uint8) *U8 {
+	return (*U8)(unsafe.Pointer(addr))
 }
 
-func (r *Reg8) ClearBit(n int) {
-	r.r &^= uint8(1) << uint(n)
+func (r *U8) SetBit(n int) {
+	r.r |= uint8(1 << uint(n))
 }
 
-func (r *Reg8) Bit(n int) bool {
-	return r.r&(uint8(1)<<uint(n)) != 0
+func (r *U8) ClearBit(n int) {
+	r.r &^= uint8(1 << uint(n))
 }
 
-func (r *Reg8) StoreBits(bits, mask uint8) {
+func (r *U8) Bit(n int) bool {
+	return r.r&uint8(1<<uint(n)) != 0
+}
+
+func (r *U8) StoreBits(bits, mask uint8) {
 	r.r = r.r&^mask | bits
 }
 
-func (r *Reg8) LoadBits(mask uint8) uint8 {
+func (r *U8) SetBits(mask uint8) {
+	r.r |= mask
+}
+
+func (r *U8) ClearBits(mask uint8) {
+	r.r &^= mask
+}
+
+func (r *U8) LoadBits(mask uint8) uint8 {
 	return r.r & mask
 }
 
-func (r *Reg8) Load() uint8 {
+func (r *U8) Load() uint8 {
 	return r.r
 }
 
-func (r *Reg8) Store(v uint8) {
+func (r *U8) Store(v uint8) {
 	r.r = v
 }
 
-type Reg16 struct {
+func (r *U8) Ptr() uintptr {
+	return uintptr(unsafe.Pointer(r))
+}
+
+type U16 struct {
 	r uint16
 } //c:volatile
 
-func PtrReg16(addr uintptr) *Reg16 {
-	return (*Reg16)(unsafe.Pointer(addr))
+func PtrU16(addr unsafe.Pointer) *U16 {
+	return (*U16)(addr)
 }
 
-func (r *Reg16) SetBit(n int) {
-	r.r |= uint16(1) << uint(n)
+func AsU16(addr *uint16) *U16 {
+	return (*U16)(unsafe.Pointer(addr))
 }
 
-func (r *Reg16) ClearBit(n int) {
-	r.r &^= uint16(1) << uint(n)
+func (r *U16) SetBit(n int) {
+	r.r |= uint16(1 << uint(n))
 }
 
-func (r *Reg16) Bit(n int) bool {
-	return r.r&(uint16(1)<<uint(n)) != 0
+func (r *U16) ClearBit(n int) {
+	r.r &^= uint16(1 << uint(n))
 }
 
-func (r *Reg16) StoreBits(bits, mask uint16) {
+func (r *U16) Bit(n int) bool {
+	return r.r&uint16(1<<uint(n)) != 0
+}
+
+func (r *U16) StoreBits(bits, mask uint16) {
 	r.r = r.r&^mask | bits
 }
 
-func (r *Reg16) LoadBits(mask uint16) uint16 {
+func (r *U16) SetBits(mask uint16) {
+	r.r |= mask
+}
+
+func (r *U16) ClearBits(mask uint16) {
+	r.r &^= mask
+}
+
+func (r *U16) LoadBits(mask uint16) uint16 {
 	return r.r & mask
 }
 
-func (r *Reg16) Load() uint16 {
+func (r *U16) Load() uint16 {
 	return r.r
 }
 
-func (r *Reg16) Store(v uint16) {
+func (r *U16) Store(v uint16) {
 	r.r = v
 }
 
-type Reg32 struct {
+func (r *U16) Ptr() uintptr {
+	return uintptr(unsafe.Pointer(r))
+}
+
+type U32 struct {
 	r uint32
 } //c:volatile
 
-func PtrReg32(addr uintptr) *Reg32 {
-	return (*Reg32)(unsafe.Pointer(addr))
+func PtrU32(addr unsafe.Pointer) *U32 {
+	return (*U32)(addr)
 }
 
-func (r *Reg32) SetBit(n int) {
-	r.r |= uint32(1) << uint(n)
+func AsU32(addr *uint32) *U32 {
+	return (*U32)(unsafe.Pointer(addr))
 }
 
-func (r *Reg32) ClearBit(n int) {
-	r.r &^= uint32(1) << uint(n)
+func (r *U32) SetBit(n int) {
+	r.r |= uint32(1 << uint(n))
 }
 
-func (r *Reg32) Bit(n int) bool {
-	return r.r&(uint32(1)<<uint(n)) != 0
+func (r *U32) ClearBit(n int) {
+	r.r &^= uint32(1 << uint(n))
 }
 
-func (r *Reg32) LoadBits(mask uint32) uint32 {
+func (r *U32) Bit(n int) bool {
+	return r.r&uint32(1<<uint(n)) != 0
+}
+
+func (r *U32) LoadBits(mask uint32) uint32 {
 	return r.r & mask
 }
 
-func (r *Reg32) StoreBits(bits, mask uint32) {
+func (r *U32) StoreBits(bits, mask uint32) {
 	r.r = r.r&^mask | bits
 }
 
-func (r *Reg32) Load() uint32 {
+func (r *U32) SetBits(mask uint32) {
+	r.r |= mask
+}
+
+func (r *U32) ClearBits(mask uint32) {
+	r.r &^= mask
+}
+
+func (r *U32) Load() uint32 {
 	return r.r
 }
 
-func (r *Reg32) Store(v uint32) {
+func (r *U32) Store(v uint32) {
 	r.r = v
+}
+
+func (r *U32) Ptr() uintptr {
+	return uintptr(unsafe.Pointer(r))
 }
