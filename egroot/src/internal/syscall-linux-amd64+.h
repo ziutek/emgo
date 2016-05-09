db72566be6812144d40a21b890ec89016672bcaf
Michał Derkacz <michal@Lnet.pl>
2016-05-09 18:18:56 +0200

    syscall: Fixes bug in registers arguments for SVC.
    In case of macro ensure that all arguments are evalueted before assign
    them to specific registers.

--- egroot/src/internal/syscall-linux-amd64+.h
+++ egroot/src/internal/syscall-linux-amd64+.h
@@ -8,7 +8,7 @@ internal$Syscall1(uintptr trap, uintptr a1) {
 	asm volatile (
 		"syscall"
 		:"+r" (rax)
-		:"r" (rdi)
+		:"r"(rdi)
 		:"%rcx", "%r11", "memory"
 	);
 	return rax;
@@ -22,7 +22,7 @@ internal$Syscall2(uintptr trap, uintptr a1, uintptr a2) {
 	asm volatile (
 		"syscall"
 		:"+r" (rax)
-		:"r" (rdi), "r" (rsi)
+		:"r"(rdi), "r"(rsi)
 		:"%rcx", "%r11", "memory"
 	);
 	return rax;
@@ -30,14 +30,18 @@ internal$Syscall2(uintptr trap, uintptr a1, uintptr a2) {
 
 static inline uintptr
 internal$Syscall3(uintptr trap, uintptr a1, uintptr a2, uintptr a3) {
-	register uintptr rax asm("%rax") = trap;
-	register uintptr rdi asm("%rdi") = a1;
-	register uintptr rsi asm("%rsi") = a2;
-	register uintptr rdx asm("%rdx") = a3;
+	uintptr t0 = trap;
+	uintptr t1 = a1;
+	uintptr t2 = a2;
+	uintptr t3 = a3;
+	register uintptr rax asm("%rax") = t0;
+	register uintptr rdi asm("%rdi") = t1;
+	register uintptr rsi asm("%rsi") = t2;
+	register uintptr rdx asm("%rdx") = t3;
 	asm volatile (
 		"syscall"
 		:"+r" (rax)
-		:"r" (rdi), "r" (rsi), "r" (rdx)
+		:"r"(rdi), "r"(rsi), "r"(rdx)
 		:"%rcx", "%r11", "memory"
 	);
 	return rax;
@@ -53,42 +57,44 @@ internal$Syscall4(uintptr trap, uintptr a1, uintptr a2, uintptr a3, uintptr a4) 
 	asm volatile (
 		"syscall"
 		:"+r" (rax)
-		:"r" (rdi), "r" (rsi), "r" (rdx), "r" (r10)
+		:"r"(rdi), "r"(rsi), "r"(rdx), "r"(r10)
 		:"%rcx", "%r11", "memory"
 	);
 	return rax;
 }
 
 static inline uintptr
-internal$Syscall5(uintptr trap, uintptr a1, uintptr a2, uintptr a3, uintptr a4, uintptr a5) {
+internal$Syscall5(uintptr trap, uintptr a1, uintptr a2, uintptr a3, uintptr a4,
+	uintptr a5) {
 	register uintptr rax asm("%rax") = trap;
 	register uintptr rdi asm("%rdi") = a1;
 	register uintptr rsi asm("%rsi") = a2;
 	register uintptr rdx asm("%rdx") = a3;
 	register uintptr r10 asm("%r10") = a4;
-	register uintptr r8  asm("%r8")  = a5;
+	register uintptr r8 asm("%r8") = a5;
 	asm volatile (
 		"syscall"
 		:"+r" (rax)
-		:"r" (rdi), "r" (rsi), "r" (rdx), "r" (r10), "r" (r8)
+		:"r"(rdi), "r"(rsi), "r"(rdx), "r"(r10), "r"(r8)
 		:"%rcx", "%r11", "memory"
 	);
 	return rax;
 }
 
 static inline uintptr
-internal$Syscall6(uintptr trap, uintptr a1, uintptr a2, uintptr a3, uintptr a4, uintptr a5, uintptr a6) {
+internal$Syscall6(uintptr trap, uintptr a1, uintptr a2, uintptr a3, uintptr a4,
+	uintptr a5, uintptr a6) {
 	register uintptr rax asm("%rax") = trap;
 	register uintptr rdi asm("%rdi") = a1;
 	register uintptr rsi asm("%rsi") = a2;
 	register uintptr rdx asm("%rdx") = a3;
 	register uintptr r10 asm("%r10") = a4;
-	register uintptr r8  asm("%r8")  = a5;
-	register uintptr r9  asm("%r9")  = a6;
+	register uintptr r8 asm("%r8") = a5;
+	register uintptr r9 asm("%r9") = a6;
 	asm volatile (
 		"syscall"
 		:"+r" (rax)
-		:"r" (rdi), "r" (rsi), "r" (rdx), "r" (r10), "r" (r8), "r" (r9)
+		:"r"(rdi), "r"(rsi), "r"(rdx), "r"(r10), "r"(r8), "r"(r9)
 		:"%rcx", "%r11", "memory"
 	);
 	return rax;
--- egroot/src/internal/syscall-noos-cortexm+.h
+++ egroot/src/internal/syscall-noos-cortexm+.h
@@ -26,28 +26,33 @@
 })
 
 #define internal$Syscall2(trap, a1, a2) ({ \
-	register uintptr r0 asm("r0") = a1;   \
-	register uintptr r1 asm("r1") = a2;   \
-	asm volatile (                        \
-		"svc %2"                          \
-		: "+r" (r0), "+r" (r1)            \
-		: "i" (trap)                      \
-		: "memory"                        \
-	);                                    \
-	(uintptr$$uintptr){r0, r1};           \
+	uintptr t1 = a1;                       \
+	uintptr t2 = a2;                       \
+	register uintptr r0 asm("r0") = t1;    \
+	register uintptr r1 asm("r1") = t2;    \
+	asm volatile (                         \
+		"svc %2"                           \
+		: "+r" (r0), "+r" (r1)             \
+		: "i" (trap)                       \
+		: "memory"                         \
+	);                                     \
+	(uintptr$$uintptr){r0, r1};            \
 })
 
 #define internal$Syscall3(trap, a1, a2, a3) ({ \
-	register uintptr r0 asm("r0") = a1;       \
-	register uintptr r1 asm("r1") = a2;       \
-	register uintptr r2 asm("r2") = a3;       \
-	asm volatile (                            \
-		"svc %2"                              \
-		: "+r" (r0), "+r" (r1)                \
-		: "i" (trap), "r" (r2)                \
-		: "memory"                            \
-	);                                        \
-	(uintptr$$uintptr){r0, r1};               \
+	uintptr t1 = a1;                           \
+	uintptr t2 = a2;                           \
+	uintptr t3 = a3;                           \
+	register uintptr r0 asm("r0") = t1;        \
+	register uintptr r1 asm("r1") = t2;        \
+	register uintptr r2 asm("r2") = t3;        \
+	asm volatile (                             \
+		"svc %2"                               \
+		: "+r" (r0), "+r" (r1)                 \
+		: "i" (trap), "r" (r2)                 \
+		: "memory"                             \
+	);                                         \
+	(uintptr$$uintptr){r0, r1};                \
 })
 
 // uint64 in register
@@ -56,26 +61,26 @@
 // It seems that `register uint64 r asm("r0")` means that r ocupies r0:r1.
 
 #define internal$Syscall1i64(trap, a1) ({ \
-	register int64   r  asm("r0") = a1;  \
-	register uintptr r0 asm("r0");       \
-	register uintptr r1 asm("r1");       \
-	asm volatile (                       \
-		"svc %2"                         \
-		: "=r" (r0), "=r" (r1)           \
-		: "i" (trap), "r" (r)            \
-		: "memory"                       \
-	);                                   \
-	(uintptr$$uintptr){r0, r1};          \
+	register int64   r  asm("r0") = a1;   \
+	register uintptr r0 asm("r0");        \
+	register uintptr r1 asm("r1");        \
+	asm volatile (                        \
+		"svc %2"                          \
+		: "=r" (r0), "=r" (r1)            \
+		: "i" (trap), "r" (r)             \
+		: "memory"                        \
+	);                                    \
+	(uintptr$$uintptr){r0, r1};           \
 })
 
 
 #define internal$Syscall0r64(trap) ({ \
-	register int64 r asm("r0");      \
-	asm volatile (                   \
-		"svc %1"                     \
-		: "=r" (r)                   \
-		: "i" (trap)                 \
-		: "memory"                   \
-	);                               \
-	r;                               \
+	register int64 r asm("r0");       \
+	asm volatile (                    \
+		"svc %1"                      \
+		: "=r" (r)                    \
+		: "i" (trap)                  \
+		: "memory"                    \
+	);                                \
+	r;                                \
 })
