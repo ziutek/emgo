176ea368cf84ac8d48aa93df43fe3883022649dd
Michał Derkacz <michal@Lnet.pl>
2016-07-20 23:02:40 +0200

    io. Fixes bug in ReadFull. New ErrShortWrite error.

--- egroot/src/io/errors.go
+++ egroot/src/io/errors.go
@@ -2,4 +2,8 @@ package io
 
 import "errors"
 
-var EOF = errors.New("EOF")
+var (
+	EOF              = errors.New("EOF")
+	ErrUnexpectedEOF = errors.New("unexpected EOF")
+	ErrShortWrite    = errors.New("short write")
+)
--- egroot/src/io/utils.go
+++ egroot/src/io/utils.go
@@ -11,7 +11,7 @@ func WriteString(w Writer, s string) (int, error) {
 	return w.Write([]byte(s))
 }
 
-type discard int
+type discard byte
 
 func (_ discard) Write(b []byte) (int, error)       { return len(b), nil }
 func (_ discard) WriteString(s string) (int, error) { return len(s), nil }
@@ -19,3 +19,21 @@ func (_ discard) WriteString(s string) (int, error) { return len(s), nil }
 // Discard implements Writer and StringWriter and simply
 // discards all data written to it.
 const Discard discard = 0
+
+// ReadFull reads exactly len(buf) bytes from r into buf. It returns the
+// number of bytes copied and an error if fewer bytes were read. The error
+// is EOF only if no bytes were read. If an EOF happens after reading some
+// but not all the bytes, ReadFull returns ErrUnexpectedEOF.
+func ReadFull(r Reader, buf []byte) (n int, err error) {
+	for n < len(buf) {
+		m, err := r.Read(buf[n:])
+		n += m
+		if err != nil {
+			if err == EOF && n < len(buf) {
+				err = ErrUnexpectedEOF
+			}
+			return n, err
+		}
+	}
+	return n, nil
+}
