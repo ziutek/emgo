cc7e677582c22875dd1e78902e27d7ae9d6068e6
Michał Derkacz <michal@Lnet.pl>
2016-09-17 22:05:24 +0200

    internal: Avoid problem with inlining by iplementing NewTask in C.

--- egroot/src/internal/newtask+.c
+++ egroot/src/internal/newtask+.c
@@ -1,9 +1,9 @@
-uintptr
-internal$b2u(bool b) {
-	return (uintptr) (b);
+void
+internal$NewTask(void (*f) (), bool lock) {
+	uintptr$$uintptr r = internal$Syscall2(0, (uintptr) (f), (uintptr) (lock));
+	uintptr e = r._1;
+	if (e != 0) {
+		panic(INTERFACE(e, &uintptr$$));
+	}
 }
 
-uintptr
-internal$f2u(void (*f) ()) {
-	return (uintptr) (f);
-}
--- egroot/src/internal/newtask-noos.go
+++ egroot/src/internal/newtask-noos.go
@@ -3,13 +3,4 @@
 package internal
 
 //c:inline
-func b2u(bool) uintptr
-
-//c:inline
-func f2u(func()) uintptr
-
-func NewTask(f func(), lock bool) {
-	if _, e := Syscall2(NEWTASK, f2u(f), b2u(lock)); e != 0 {
-		panic(e)
-	}
-}
+func NewTask(f func(), lock bool)
