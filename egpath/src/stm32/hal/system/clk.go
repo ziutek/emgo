fd3ab6cd1362fd6e6486f2266b1b20f2533d1e0b
Michał Derkacz <michal@Lnet.pl>
2016-10-18 22:48:19 +0200

    stm32/hal/system: New Bus.String method.

--- egpath/src/stm32/hal/system/clk.go
+++ egpath/src/stm32/hal/system/clk.go
@@ -1,6 +1,6 @@
 package system
 
-type Bus int
+type Bus int8
 
 const (
 	Core Bus = iota
@@ -15,4 +15,19 @@ var clock [busN]uint
 // Clock returns clock frequency [Hz] for bus.
 func (b Bus) Clock() uint {
 	return clock[b]
+}
+
+//emgo:const
+var busStr = [...]string{
+	Core: "Core",
+	AHB:  "AHB",
+	APB1: "APB1",
+	APB2: "APB2",
+}
+
+func (b Bus) String() string {
+	if uint(b) >= uint(busN) {
+		return ""
+	}
+	return busStr[b]
 }
