c812dade67971a1481cf0dd00192d8eed5494060
Michał Derkacz <michal@Lnet.pl>
2016-10-28 10:08:40 +0200

    stm32/examples: New RC servo examples.

---
+++ egpath/src/stm32/examples/minidev/nrfrc/main.go
@@ -0,0 +1,165 @@
+// This is example of nRF24L01 based RC transmiter.
+// See ../../nucleo-f411re/rcservo that can be used on receiver side.
+package main
+
+import (
+	"delay"
+	"fmt"
+	"mmio"
+	"rtos"
+
+	"arch/cortexm/bitband"
+
+	"nrf24"
+
+	"stm32/nrfdci"
+
+	"stm32/hal/dma"
+	"stm32/hal/exti"
+	"stm32/hal/gpio"
+	"stm32/hal/irq"
+	"stm32/hal/spi"
+	"stm32/hal/system"
+	"stm32/hal/system/timer/rtc"
+
+	"stm32/hal/raw/rcc"
+	"stm32/hal/raw/tim"
+)
+
+var (
+	led bitband.Bit
+	dci *nrfdci.DCI
+	enc *mmio.U16
+)
+
+func init() {
+	system.Setup(8, 72/8, false)
+	rtc.Setup(32768)
+	start := rtos.Nanosec()
+
+	gpio.A.EnableClock(true)
+	spiport, csn, sck, miso, mosi := gpio.A, gpio.Pin4, gpio.Pin5, gpio.Pin6, gpio.Pin7
+
+	gpio.B.EnableClock(true)
+	ctrport, ce, irqn := gpio.B, gpio.Pin0, gpio.Pin1
+	encport, encpins := gpio.B, gpio.Pin6|gpio.Pin7
+
+	gpio.C.EnableClock(false)
+	ledport, ledpin := gpio.C, 13
+
+	// LED
+
+	ledport.SetupPin(
+		ledpin,
+		&gpio.Config{Mode: gpio.Out, Driver: gpio.OpenDrain, Speed: gpio.Low},
+	)
+	led = ledport.OutPins().Bit(ledpin)
+	led.Set()
+
+	// Encoder
+
+	encport.Setup(encpins, &gpio.Config{Mode: gpio.AltIn, Pull: gpio.PullUp})
+	rcc.RCC.TIM4EN().Set()
+	t := tim.TIM4
+	t.SMCR.Store(1 << tim.SMSn)
+	t.CCMR1.Store(tim.CC1S | tim.CC2S | 0xf<<tim.IC1Fn | 0xf<<tim.IC2Fn)
+	t.CCER.Store(tim.CC1P | tim.CC2P)
+	t.CR1.Store(2<<tim.CKDn | tim.CEN)
+	enc = &t.CNT.U16
+
+	// nRF24 SPI
+
+	spiport.Setup(sck|mosi, &gpio.Config{Mode: gpio.Alt, Speed: gpio.High})
+	spiport.Setup(miso, &gpio.Config{Mode: gpio.AltIn})
+	spiport.Setup(csn, &gpio.Config{Mode: gpio.Out, Speed: gpio.High})
+	d := dma.DMA1
+	d.EnableClock(true)
+	spid := spi.NewDriver(spi.SPI1, d.Channel(2, 0), d.Channel(3, 0))
+	spid.P.EnableClock(true)
+	rtos.IRQ(irq.SPI1).Enable()
+	rtos.IRQ(irq.DMA1_Channel2).Enable()
+	rtos.IRQ(irq.DMA1_Channel3).Enable()
+
+	// nRF24 control lines.
+
+	ctrport.Setup(ce, &gpio.Config{Mode: gpio.Alt, Speed: gpio.High})
+	rcc.RCC.TIM3EN().Set()
+	ctrport.Setup(irqn, &gpio.Config{Mode: gpio.In, Pull: gpio.PullUp})
+	irqline := exti.Lines(irqn)
+	irqline.Connect(ctrport)
+	rtos.IRQ(irq.EXTI1).Enable()
+
+	dci = nrfdci.NewDCI(
+		spid, spiport, csn, system.APB1.Clock(), tim.TIM3, 3, irqline,
+	)
+	// nRF24 requires wait at least 100 ms from start before use it.
+	rtos.SleepUntil(start + 100e6)
+
+}
+
+func checkErr(err error) {
+	if err == nil {
+		return
+	}
+	fmt.Printf("Error: %v\n", err)
+	for {
+	}
+}
+
+func main() {
+	nrf := nrf24.NewRadio(dci)
+	nrf.Set_RF_CH(108)
+	nrf.Set_RF_SETUP(nrf24.RF_DR_LOW | nrf24.RF_PWR(0))
+	nrf.Set_EN_AA(0)
+	nrf.Set_SETUP_AW(3)
+	nrf.FLUSH_TX()
+	nrf.Set_CONFIG(nrf24.PWR_UP | nrf24.EN_CRC | nrf24.CRCO)
+
+	// Wait for transition from Power Down to Standby I.
+	delay.Millisec(5)
+
+	for {
+		cnt := int16(enc.Load())
+		switch {
+		case cnt < -64:
+			cnt = -64
+			enc.Store(uint16(cnt))
+		case cnt > 64:
+			cnt = 64
+			enc.Store(uint16(cnt))
+		}
+		nrf.W_TX_PAYLOAD([]byte{byte(cnt), 0})
+		nrf.ClearIRQ(nrf24.TX_DS)
+		dci.SetCE(2)
+		dci.Wait(0)
+		delay.Millisec(40)
+	}
+}
+
+func exti1ISR() {
+	exti.L1.ClearPending()
+	dci.ISR()
+}
+
+func nrfSPIISR() {
+	dci.SPI().ISR()
+}
+
+func nrfRxDMAISR() {
+	dci.SPI().DMAISR(dci.SPI().RxDMA)
+}
+
+func nrfTxDMAISR() {
+	dci.SPI().DMAISR(dci.SPI().TxDMA)
+}
+
+//emgo:const
+//c:__attribute__((section(".ISRs")))
+var ISRs = [...]func(){
+	irq.RTCAlarm: rtc.ISR,
+
+	irq.EXTI1:         exti1ISR,
+	irq.SPI1:          nrfSPIISR,
+	irq.DMA1_Channel2: nrfRxDMAISR,
+	irq.DMA1_Channel3: nrfTxDMAISR,
+}
---
+++ egpath/src/stm32/examples/nucleo-f411re/rcservo/main.go
@@ -0,0 +1,186 @@
+// This is example of nRF24L01 based remote servo.
+// See ../../minidev/nrfrc for transmiter application that can controll it.
+package main
+
+import (
+	"delay"
+	"fmt"
+	"mmio"
+	"rtos"
+
+	"nrf24"
+
+	"stm32/nrfdci"
+
+	"stm32/hal/dma"
+	"stm32/hal/exti"
+	"stm32/hal/gpio"
+	"stm32/hal/irq"
+	"stm32/hal/spi"
+	"stm32/hal/system"
+	"stm32/hal/system/timer/systick"
+
+	"stm32/hal/raw/rcc"
+	"stm32/hal/raw/tim"
+)
+
+var (
+	dci *nrfdci.DCI
+	pwm *mmio.U32
+)
+
+func init() {
+	system.Setup96(8)
+	systick.Setup()
+	start := rtos.Nanosec()
+
+	// GPIO
+
+	gpio.A.EnableClock(true)
+	spiport, sck, miso, mosi := gpio.A, gpio.Pin5, gpio.Pin6, gpio.Pin7
+	pwmport, pwmpin := gpio.A, gpio.Pin0
+
+	gpio.B.EnableClock(true)
+	ctrport, csn, irqn, ce := gpio.B, gpio.Pin6, gpio.Pin8, gpio.Pin9
+
+	// PWM
+
+	pwmport.Setup(pwmpin, &gpio.Config{Mode: gpio.Alt, Speed: gpio.Low})
+	pwmport.SetAltFunc(pwmpin, gpio.TIM2)
+	rcc.RCC.TIM2EN().Set()
+	t := tim.TIM2
+	const (
+		pwmmode   = 6    // Mode 1
+		pwmperiod = 20   // ms
+		pwmmax    = 20e3 // (for 20 ms period it gives 1 µs resolution).
+	)
+	pclk := system.APB1.Clock()
+	if pclk != system.AHB.Clock() {
+		pclk *= 2
+	}
+	t.PSC.U16.Store(uint16(pclk/1000*pwmperiod/pwmmax - 1))
+	t.ARR.Store(pwmmax - 1)
+	t.CCMR1.Store(pwmmode<<tim.OC1Mn | tim.OC1PE)
+	t.CCER.Store(tim.CC1E)
+	t.EGR.Store(tim.UG)
+	t.CR1.Store(tim.ARPE | tim.CEN)
+	pwm = &t.CCR1.U32
+
+	// nRF24 SPI.
+
+	spiport.Setup(sck|mosi, &gpio.Config{Mode: gpio.Alt, Speed: gpio.High})
+	spiport.Setup(miso, &gpio.Config{Mode: gpio.AltIn})
+	spiport.SetAltFunc(sck|miso|mosi, gpio.SPI1)
+	d := dma.DMA2
+	d.EnableClock(true)
+	spid := spi.NewDriver(spi.SPI1, d.Channel(2, 3), d.Channel(3, 3))
+	spid.P.EnableClock(true)
+	rtos.IRQ(irq.SPI1).Enable()
+	rtos.IRQ(irq.DMA2_Stream2).Enable()
+	rtos.IRQ(irq.DMA2_Stream3).Enable()
+
+	// nRF24 control lines.
+
+	ctrport.Setup(csn, &gpio.Config{Mode: gpio.Out, Speed: gpio.High})
+	ctrport.Setup(ce, &gpio.Config{Mode: gpio.Alt, Speed: gpio.High})
+	ctrport.SetAltFunc(ce, gpio.TIM4)
+	rcc.RCC.TIM4EN().Set()
+	ctrport.Setup(irqn, &gpio.Config{Mode: gpio.In, Pull: gpio.PullUp})
+	irqline := exti.Lines(irqn)
+	irqline.Connect(ctrport)
+	rtos.IRQ(irq.EXTI9_5).Enable()
+
+	dci = nrfdci.NewDCI(
+		spid, ctrport, csn, system.APB1.Clock(), tim.TIM4, 4, irqline,
+	)
+
+	// nRF24 requires wait at least 100 ms from start before use it.
+	rtos.SleepUntil(start + 100e6)
+}
+
+func checkErr(err error) {
+	if err == nil {
+		return
+	}
+	fmt.Printf("Error: %v\n", err)
+	for {
+	}
+}
+
+func main() {
+	// For SG92R servo (PWM: 3.3V, 20 ms).
+	const (
+		min    = 600  // µs
+		max    = 2400 // µs
+		center = (min + max) / 2
+	)
+
+	buf := make([]byte, 2)
+
+	nrf := nrf24.NewRadio(dci)
+	nrf.Set_RF_CH(108)
+	nrf.Set_RF_SETUP(nrf24.RF_DR_LOW)
+	nrf.Set_EN_AA(0)
+	nrf.Set_EN_RXADDR(nrf24.P0)
+	nrf.Set_SETUP_AW(3)
+	nrf.Set_RX_PW(0, len(buf))
+	nrf.Set_CONFIG(nrf24.PWR_UP | nrf24.EN_CRC | nrf24.CRCO | nrf24.PRIM_RX)
+
+	// Wait for transition from Power Down to Standby I.
+	delay.Millisec(5)
+
+	nrf.FLUSH_RX()
+	dci.Clear()
+	nrf.ClearIRQ(nrf24.RX_DR)
+	dci.SetCE(1)
+
+	var fs nrf24.FIFO_STATUS
+	for {
+		if fs&nrf24.RX_EMPTY != 0 {
+			dci.Wait(0)
+		}
+		nrf.R_RX_PAYLOAD(buf)
+		dci.Clear()
+		nrf.ClearIRQ(nrf24.RX_DR)
+
+		x, y := int(int8(buf[0])), int(int8(buf[1]))
+		_ = y
+		switch {
+		case x < -64:
+			x = -64
+		case x > 64:
+			x = 64
+		}
+		pwm.Store(uint32(center + x*(max-min)/128))
+		fs, _ = nrf.FIFO_STATUS()
+	}
+}
+
+func exti9_5ISR() {
+	p := exti.Pending() & (exti.L9 | exti.L8 | exti.L7 | exti.L6 | exti.L5)
+	p.ClearPending()
+	if p&dci.IRQ() != 0 {
+		dci.ISR()
+	}
+}
+
+func nrfSPIISR() {
+	dci.SPI().ISR()
+}
+
+func nrfRxDMAISR() {
+	dci.SPI().DMAISR(dci.SPI().RxDMA)
+}
+
+func nrfTxDMAISR() {
+	dci.SPI().DMAISR(dci.SPI().TxDMA)
+}
+
+//emgo:const
+//c:__attribute__((section(".ISRs")))
+var ISRs = [...]func(){
+	irq.EXTI9_5:      exti9_5ISR,
+	irq.SPI1:         nrfSPIISR,
+	irq.DMA2_Stream2: nrfRxDMAISR,
+	irq.DMA2_Stream3: nrfTxDMAISR,
+}
