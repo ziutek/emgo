d03d1cf044e1571fa698368a0181826f03100bba
Michał Derkacz <michal@Lnet.pl>
2016-12-25 23:20:36 +0100

    stm32/examples/minidev: Description.

--- egpath/src/stm32/examples/minidev/treelights/main.go
+++ egpath/src/stm32/examples/minidev/treelights/main.go
@@ -47,7 +47,7 @@ func init() {
 	leds = usart.NewDriver(usart.USART2, nil, d.Channel(7, 0), nil)
 	leds.P.EnableClock(true)
 	leds.P.SetBaudRate(2250e3)    // 36MHz/16: 444 ns/UARTbit, 1333 ns/WS2811bit.
-	leds.P.SetConf(usart.Stop0b5) // F103 has no 7-bit mode: save 0.5 bit only.
+	leds.P.SetConf(usart.Stop0b5) // F103 has no 7-bit mode: save 0.5 bit.
 	leds.P.Enable()
 	leds.EnableTx()
 	rtos.IRQ(irq.USART2).Enable()
@@ -76,7 +76,7 @@ func init() {
 	audioport.Setup(audiopins, &gpio.Config{Mode: gpio.Alt, Speed: gpio.Low})
 	rcc.RCC.TIM4EN().Set()
 	// For TIMclk=72Mhz and sr=14700, max=136 gives PSC=9-1 and actual SR=14706.
-	audio.Setup(tim.TIM4, system.APB1.Clock(), 14700, 136*3)
+	audio.Setup(tim.TIM4, system.APB1.Clock(), 14700, 136*3) // max*3 => gain/3.
 	rtos.IRQ(irq.TIM4).Enable()
 
 	// AUX output
@@ -106,10 +106,14 @@ func play(melody []Note, n int) {
 			d := note.Duration
 			snd := note.Sample[:]
 			dly := 0
+			// Full snd and duration 4 correspond to quarter note (650 ms).
+			const Q = 650
 			if d < 4 {
+				// Shorten snd.
 				snd = snd[:(len(snd)*d+2)/4]
-			} else {
-				dly = (650*d+2)/4 - 650
+			} else if d > 4 {
+				// Apply some delay after snd.
+				dly = (Q*d+2)/4 - Q
 			}
 			audio.Play(snd)
 			delay.Millisec(dly)
@@ -131,65 +135,73 @@ func main() {
 	var color ws281x.Color
 	color1 := rndColor()
 	color2 := rndColor()
-	x := 14000
-	k := 0
+	dist := 14000 // 14 m
+	iter := 0
 	for {
+		// Read distance from ultrasonic sensor.
 		checkErr(us100.WriteByte(0x55))
 		_, err := io.ReadFull(us100, buf)
 		checkErr(err)
-		x = (x + int(buf[0])<<8 + int(buf[1])) / 2
+		dist = (dist + int(buf[0])<<8 + int(buf[1])) / 2
 
-		switch {
-		case x < 1500:
-			x = 14000
+		if dist < 1500 {
+			dist = 14000
+
+			// Slowly dim the current color.
 			r, g, b := color.RGB()
-			for n := 256; n >= 0; n-- {
-				pixel.EncodeRGB(ws281x.RGB(r*n/256, g*n/256, b*n/256).Gamma())
+			const N = 256
+			for i := N; i >= 0; i-- {
+				pixel.EncodeRGB(ws281x.RGB(r*i/N, g*i/N, b*i/N).Gamma())
 				ledram.Fill(pixel)
 				leds.Write(ledram.Bytes())
 				delay.Millisec(6)
 			}
 			delay.Millisec(500)
 
+			// Light the red color and play music.
 			ledram.Fill(red)
 			leds.Write(ledram.Bytes())
-			if k&1 == 0 {
-				play(melody1, 1)
+			if iter&1 == 0 {
+				play(melody1, 2)
 			} else {
 				play(melody2, 3)
 			}
-			for n := 0; n < 25; n++ {
-				ledram.At(n).Head(1).Clear()
-				ledram.At(49-n).Head(1).Clear()
+
+			// Turn off LEDs in sequence (starting from both ends).
+			for i := 0; i < ledram.Len()/2; i++ {
+				ledram.At(i).Head(1).Clear()
+				ledram.At(49 - i).Head(1).Clear()
 				leds.Write(ledram.Bytes())
 				delay.Millisec(10)
 			}
 			delay.Millisec(400)
+		}
 
-		default:
-			const N = 64
-			r := (color1.Red()*(N-k) + color2.Red()*k) / N
-			g := (color1.Green()*(N-k) + color2.Green()*k) / N
-			b := (color1.Blue()*(N-k) + color2.Blue()*k) / N
-			color = ws281x.RGB(r, g, b)
-			if k++; k > N {
-				k = 0
-				color1 = color2
-				color2 = rndColor()
-			}
-			pixel.EncodeRGB(color.Gamma())
-			ledram.Fill(pixel)
-			leds.Write(ledram.Bytes())
-			for n := 0; n < 10; n++ {
-				if r := int(rnd.Uint32() & 0x1ff); r < ledram.Len() {
-					ledram.At(r).Write(white)
-					leds.Write(ledram.Bytes())
-					delay.Millisec(20)
-					ledram.At(r).Write(pixel)
-					leds.Write(ledram.Bytes())
-				} else {
-					delay.Millisec(60)
-				}
+		// Slowly change the current color.
+		const N = 64
+		r := (color1.Red()*(N-iter) + color2.Red()*iter) / N
+		g := (color1.Green()*(N-iter) + color2.Green()*iter) / N
+		b := (color1.Blue()*(N-iter) + color2.Blue()*iter) / N
+		color = ws281x.RGB(r, g, b)
+		if iter++; iter > N {
+			iter = 0
+			color1 = color2
+			color2 = rndColor()
+		}
+		pixel.EncodeRGB(color.Gamma())
+		ledram.Fill(pixel)
+		leds.Write(ledram.Bytes())
+
+		// Sparkle effect.
+		for i := 0; i < 10; i++ {
+			if r := int(rnd.Uint32() & 0x1ff); r < ledram.Len() {
+				ledram.At(r).Write(white)
+				leds.Write(ledram.Bytes())
+				delay.Millisec(20)
+				ledram.At(r).Write(pixel)
+				leds.Write(ledram.Bytes())
+			} else {
+				delay.Millisec(60)
 			}
 		}
 	}
