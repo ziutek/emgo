7f7a77ab987f373f8f30765dd7acb56ab2c4f016
Michał Derkacz <michal@Lnet.pl>
2017-03-14 23:58:16 +0100

    bytes: New Compare function.

--- egroot/src/bytes/bytes.go
+++ egroot/src/bytes/bytes.go
@@ -5,6 +5,29 @@ import (
 	"unsafe"
 )
 
+func Compare(s1, s2 []byte) int {
+	n := len(s1)
+	if n > len(s2) {
+		n = len(s2)
+	}
+	p1 := unsafe.Pointer((*internal.SliceHeader)(unsafe.Pointer(&s1)).Data)
+	p2 := unsafe.Pointer((*internal.SliceHeader)(unsafe.Pointer(&s2)).Data)
+	ret := internal.Memcmp(p1, p2, uintptr(n))
+	if ret != 0 || len(s1) == len(s2) {
+		switch {
+		case ret < 0:
+			ret = -1
+		case ret > 0:
+			ret = 1
+		}
+		return ret
+	}
+	if len(s1) < len(s2) {
+		return -1
+	}
+	return 1
+}
+
 func Equal(s1, s2 []byte) bool {
 	if len(s1) != len(s2) {
 		return false
