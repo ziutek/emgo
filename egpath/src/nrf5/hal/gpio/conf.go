e9c2fb6463203a3b6e6fc0a12739cd51a498586b
Michał Derkacz <michal@Lnet.pl>
2017-03-15 00:01:31 +0100

    nrf5/hal/gpio: More configuration options.

--- egpath/src/nrf5/hal/gpio/conf.go
+++ egpath/src/nrf5/hal/gpio/conf.go
@@ -17,13 +17,39 @@ const (
 	PullUp   Pull = 3
 )
 
+type Drive byte
+
+const (
+	S0S1 Drive = 0 // Standard 0, standard 1.
+	H0S1 Drive = 1 // High drive 0, standard 1.
+	S0H1 Drive = 2 // Standard 0, high drive 1.
+	H0H1 Drive = 3 // High drive 0, high drive 1.
+	D0S1 Drive = 4 // Disconnect 0, standard 1.
+	D0H1 Drive = 5 // Disconnect 0, high drive 1.
+	S0D1 Drive = 6 // Standard 0, disconnect 1.
+	H0D1 Drive = 7 // High drive 0, disconnect 1.
+)
+
+type Sense byte
+
+const (
+	NoSense   Sense = 0
+	SenseHigh Sense = 2
+	SenseLow  Sense = 3
+)
+
 type Config struct {
-	Mode Mode
-	Pull Pull
+	Mode  Mode
+	Pull  Pull
+	Drive Drive
+	Sense Sense
 }
 
 func (p *Port) SetupPin(n int, cfg *Config) {
-	p.pincnf[n].Store(uint32(cfg.Pull)<<2 | uint32(cfg.Mode))
+	p.pincnf[n].Store(
+		uint32(cfg.Sense)<<16 | uint32(cfg.Drive)<<8 | uint32(cfg.Pull)<<2 |
+			uint32(cfg.Mode),
+	)
 }
 
 // Setup configures pins.
@@ -33,4 +59,14 @@ func (p *Port) Setup(pins Pins, cfg *Config) {
 			p.SetupPin(n, cfg)
 		}
 	}
+}
+
+// SetDirIn allows a fast change of direction to input for specified pins.
+func (p *Port) SetDirIn(pins Pins) {
+	p.dirset.Store(uint32(pins))
+}
+
+// SetDirOut allows afast change of direction to output for specified pins.
+func (p *Port) SetDirOut(pins Pins) {
+	p.dirclr.Store(uint32(pins))
 }
