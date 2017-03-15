b2a20001524010df48a1b69f10444bcddce8e07e
Michał Derkacz <michal@Lnet.pl>
2017-03-15 13:49:35 +0100

    nrf5/hal/gpio: Remove unused code.

--- egpath/src/nrf5/hal/gpio/ctrl.go
+++ egpath/src/nrf5/hal/gpio/ctrl.go
@@ -34,25 +34,3 @@ func (p *Port) LoadOut() Pins {
 func (p *Port) Store(val Pins) {
 	p.out.Store(uint32(val))
 }
-
-/*
-// InPin returns current value of n-th input pin.
-func (p *Port) InPin(n int) int {
-	return int(p.in.Load()>>uint(n)) & 1
-}
-
-// OutPin returns current value of n-th output pin.
-func (p *Port) OutPin(n int) int {
-	return int(p.out.Load()>>uint(n)) & 1
-}
-
-// SetPin sets output value of n-th pin to 1.
-func (p *Port) SetPin(n int) {
-	p.outset.Store(1 << uint(n))
-}
-
-// ClearPin sets output value of n-th pin to 0.
-func (p *Port) ClearPin(n int) {
-	p.outclr.Store(1 << uint(n))
-}
-*/
