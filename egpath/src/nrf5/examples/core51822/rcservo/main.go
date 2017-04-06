804e481cf830f6de6dfad84b7caa961760422135
Michał Derkacz <michal@Lnet.pl>
2017-04-06 17:27:24 +0200

    examples/core51822/rcservo: Working version.

--- egpath/src/nrf5/examples/core51822/rcservo/main.go
+++ egpath/src/nrf5/examples/core51822/rcservo/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"delay"
 	"fmt"
 	"rtos"
+	"sync/fence"
+	"unsafe"
 
 	"nrf5/ppipwm"
 
@@ -12,6 +13,7 @@ import (
 	"nrf5/hal/gpiote"
 	"nrf5/hal/irq"
 	"nrf5/hal/ppi"
+	"nrf5/hal/radio"
 	"nrf5/hal/rtc"
 	"nrf5/hal/system"
 	"nrf5/hal/system/timer/rtcst"
@@ -20,8 +22,9 @@ import (
 )
 
 var (
-	pwm *ppipwm.Toggle
-	u   *uart.Driver
+	pwm        *ppipwm.Toggle
+	u          *uart.Driver
+	radioEvent rtos.EventFlag
 )
 
 func init() {
@@ -35,27 +38,65 @@ func init() {
 	u.P.StorePSEL(uart.SignalTXD, p0.Pin(9))
 	u.P.StoreBAUDRATE(uart.Baud115200)
 	u.P.StoreENABLE(true)
-	rtos.IRQ(irq.UART0).Enable()
+	rtos.IRQ(u.P.IRQ()).Enable()
 	u.EnableTx()
 	fmt.DefaultWriter = u
 
+	r := radio.RADIO
+	r.StorePCNF0(0)
+	r.StorePCNF1(
+		radio.MaxLen(2) | radio.StatLen(2) | radio.BALen(2) | radio.MSBFirst,
+	)
+	r.StoreCRCCNF(2, false)
+	r.StoreCRCPOLY(1<<16 | 1<<12 | 1<<5 | 1)
+	r.StoreCRCINIT(0xFFFF)
+	r.StoreBASE(0, 0xE7E70000) // Reversed 0xE7E70000.
+	r.StorePREFIX(0, 0xE7)     // Reversed 0xE7.
+	r.StoreRXADDRESSES(1 << 0)
+	r.StoreMODE(radio.NRF_250K)
+	r.StoreFREQUENCY(radio.Channel(50))
+	r.StoreSHORTS(radio.READY_START)
+	rtos.IRQ(r.IRQ()).Enable()
+
 	pwm = ppipwm.NewToggle(timer.TIMER1)
-	pwm.SetFreq(7, 20e3) // Gives 2500 levels of duty cycle.
+	pwm.SetFreq(8, 20e3) // Gives resolution of 1250 levels of duty cycle.
 	pwm.Setup(0, p0.Pin(22), gpiote.Chan(0), ppi.Chan(0), ppi.Chan(1))
 }
 
 func main() {
-	min := pwm.Max() * 600 / 20e3
-	max := pwm.Max() * 2400 / 20e3
-	v, dir := min, -1
+	// For SG92R servo (PWM: 3.3V, 20 ms).
+	var (
+		min    = pwm.Max() * 600 / 20e3
+		max    = pwm.Max() * 2400 / 20e3
+		center = (min + max) / 2
+	)
+
+	data := make([]byte, 2)
+
+	r := radio.RADIO
+	r.StorePACKETPTR(unsafe.Pointer(&data[0]))
+
+	start := r.Task(radio.RXEN)
 	for {
-		fmt.Printf("%d/%d\r\n", v, pwm.Max())
-		pwm.SetDutyCycle(0, v)
-		if v <= min || v >= max {
-			dir = -dir
+		endev := r.Event(radio.END)
+		endev.Clear()
+		endev.EnableIRQ()
+		radioEvent.Reset(0)
+		fence.W()
+		start.Trigger()
+		radioEvent.Wait(1, 0)
+
+		x, y := int(int8(data[0])), int(int8(data[1]))
+		fmt.Printf("x=%d y=%d\r\n", x, y)
+		switch {
+		case x < -64:
+			x = -64
+		case x > 64:
+			x = 64
 		}
-		v += dir
-		delay.Millisec(20)
+		pwm.SetDC(0, center+x*(max-min)/128)
+
+		start = r.Task(radio.START)
 	}
 }
 
@@ -63,9 +104,15 @@ func uartISR() {
 	u.ISR()
 }
 
+func radioISR() {
+	radio.RADIO.DisableIRQ(0xFFFFFFFF)
+	radioEvent.Signal(1)
+}
+
 //emgo:const
 //c:__attribute__((section(".ISRs")))
 var ISRs = [...]func(){
 	irq.RTC0:  rtcst.ISR,
 	irq.UART0: uartISR,
+	irq.RADIO: radioISR,
 }
