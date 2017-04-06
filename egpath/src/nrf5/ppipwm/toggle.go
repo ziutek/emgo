6a6433e305a4d2415046a89843ad547dfb7c46d4
Michał Derkacz <michal@Lnet.pl>
2017-04-06 17:26:23 +0200

    nrf5/ppipwm: Improve docs. Rename SetDutyCycle to SetDC and add SetInvDC method.

--- egpath/src/nrf5/ppipwm/toggle.go
+++ egpath/src/nrf5/ppipwm/toggle.go
@@ -1,5 +1,5 @@
-// Package ppipwm provides implementations of Pulse Width Modulation that uses
-// PPI to connect compare events of TIMER peripheral to GPIOTE tasks.
+// Package ppipwm allows to produce PWM signal using PPI, TIMER and GPIOTE
+// peripherals.
 package ppipwm
 
 import (
@@ -9,9 +9,20 @@ import (
 	"nrf5/hal/timer"
 )
 
-// Toggle implementats three channel PWM based on gpiote OUT task configured in
+// Toggle implementats three channel PWM usind GPIOTE OUT task configured in
 // toggle mode. It is designed specifically for nRF51 chips because of
 // limitations of their GPIOTE peripheral. Better solutions exists for nRF52.
+//
+// To produce desired PWM waveform Toggle configures a timer compare register,
+// using simple algorithm, that usually generates glitches when used on working
+// PWM channel. After that, PWM works without any CPU intervention and produces
+// proper waveform until next duty cycle change. This is fine to drive LEDs but
+// can cause trubles in case of receivers that rely on stable PWM frequency or
+// phase (eg. some servos). Correct implementation is difficult and quite
+// expensive in case of nRF51.
+//
+// Toggle cannot be used concurently by multiple gorutines without proper
+// synchronisation.
 type Toggle struct {
 	t  *timer.Periph
 	gc [3]gpiote.Chan
@@ -33,8 +44,10 @@ func NewToggle(t *timer.Periph) *Toggle {
 	return pwm
 }
 
-// SetFreq sets prescaler to 2^log2pre and period to periodus microseconds.
-// SetFreq returns a value that represents 100% duty cycle.
+// SetFreq sets prescaler to 2^log2pre and period to periodus microseconds. It
+// allows to configure a duty cycle with resolution = 16 * periodus / 2^log2pre.
+// SetFreq returns (resolution-1), which is a value that should be passed to
+// SetDC/SetInvDC to produce PWM with 100% duty cycle.
 func (pwm *Toggle) SetFreq(log2pre, periodus int) int {
 	if uint(log2pre) > 9 {
 		panic("pwm: bad prescaler")
@@ -53,18 +66,22 @@ func (pwm *Toggle) SetFreq(log2pre, periodus int) int {
 	return int(max)
 }
 
-//  Max returns a value that represents 100% duty cycle.
+// Max returns a value that corresponds to 100% PWM duty cycle. See SetFreq for
+// more information.
 func (pwm *Toggle) Max() int {
 	return int(pwm.t.LoadCC(3))
 }
 
-const badOutput = "pwm: bad output"
-
-// Setup setups one of three PWM channels.
-func (pwm *Toggle) Setup(n int, pin gpio.Pin, gc gpiote.Chan, pc0, pc1 ppi.Chan) {
+func checkOutput(n int) {
 	if uint(n) > 2 {
-		panic(badOutput)
+		panic("pwm: bad output")
 	}
+}
+
+// Setup setups n-th of three PWM channels. Each PWM channel uses one GPIOTE
+// channel and two PPI channels.
+func (pwm *Toggle) Setup(n int, pin gpio.Pin, gc gpiote.Chan, pc0, pc1 ppi.Chan) {
+	checkOutput(n)
 	pin.Clear()
 	pin.Setup(gpio.ModeOut)
 	gc.Setup(pin, 0)
@@ -78,14 +95,9 @@ func (pwm *Toggle) Setup(n int, pin gpio.Pin, gc gpiote.Chan, pc0, pc1 ppi.Chan)
 	pc1.Enable()
 }
 
-// SetDutyCycle for one of three PWM channels.
-func (pwm *Toggle) SetDutyCycle(n, dc int) {
-	// This is simple implementation that works well with LEDs but sometimes
-	// produces glitches that are detected by receivers that rely on stable PWM
-	// frequency/phase (eg. servos).
-	if uint(n) > 2 {
-		panic(badOutput)
-	}
+// SetDC sets a duty cycle for n-th PWM channel.
+func (pwm *Toggle) SetDC(n, dc int) {
+	checkOutput(n)
 	gc := pwm.gc[n]
 	t := pwm.t
 	pin, _ := gc.Config()
@@ -107,6 +119,36 @@ func (pwm *Toggle) SetDutyCycle(n, dc int) {
 		gc.Setup(pin, cfg|gpiote.OutInitHigh)
 	} else {
 		gc.Setup(pin, cfg|gpiote.OutInitLow)
+	}
+	t.StoreCC(n, uint32(dc))
+	t.Task(timer.START).Trigger()
+}
+
+// SetInvDC sets a duty cycle for n-th PWM channel. It produces inverted
+// waveform.
+func (pwm *Toggle) SetInvDC(n, dc int) {
+	checkOutput(n)
+	gc := pwm.gc[n]
+	t := pwm.t
+	pin, _ := gc.Config()
+	t.Task(timer.STOP).Trigger()
+	switch {
+	case dc <= 0:
+		pin.Set()
+		gc.Setup(pin, 0)
+		return
+	case dc >= pwm.Max():
+		pin.Clear()
+		gc.Setup(pin, 0)
+		return
+	}
+	cfg := gpiote.ModeTask | gpiote.PolarityToggle
+	t.CAPTURE(n).Trigger()
+	cnt := int(t.LoadCC(n))
+	if cnt < dc {
+		gc.Setup(pin, cfg|gpiote.OutInitLow)
+	} else {
+		gc.Setup(pin, cfg|gpiote.OutInitHigh)
 	}
 	t.StoreCC(n, uint32(dc))
 	t.Task(timer.START).Trigger()
