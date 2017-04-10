7572d99c3bd1f27cc51ae41094f490678328ce4f
Michał Derkacz <michal@Lnet.pl>
2017-04-10 12:51:51 +0200

    nrf5/examples: Replace symlinks to debug-bmp.sh, load-bmp.sh.

--- egpath/src/nrf5/examples/utils/load-bmp.sh
+++ egpath/src/nrf5/examples/utils/load-bmp.sh
@@ -1,1 +1,21 @@
-../../../stm32/examples/utils/load-bmp.sh
+#!/bin/sh
+# gdb + Black Magic Probe
+
+set -e
+
+arch=`grep 'EGARCH=' ../build.sh |sed 's/.*EGARCH=\([[:alnum:]_]\+\).*/\1/g'`
+if [ -z "$arch" ]; then
+	arch=$EGARCH
+fi
+
+arm-none-eabi-gdb \
+	-ex 'set pagination off' \
+	-ex 'set confirm off' \
+	-ex 'target extended-remote /dev/ttyACM0' \
+	-ex 'monitor connect_srst enable' \
+	-ex 'monitor swdp_scan' \
+	-ex 'attach 1' \
+	-ex 'load' \
+	-ex 'kill' \
+	-ex 'quit' \
+	$arch.elf
