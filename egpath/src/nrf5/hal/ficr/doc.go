6919017f7183dcdb2f3cb13514201efadefc3809
Michał Derkacz <michal@Lnet.pl>
2017-04-18 21:31:58 +0200

    nrf5/hal/ficr: Add package description.

---
+++ egpath/src/nrf5/hal/ficr/doc.go
@@ -0,0 +1,4 @@
+// Package ficr provides access to Factory Information Configuration Registers.
+// These registers are pre-programmed in factory and contain chip specific
+// information and configuration.
+package ficr
