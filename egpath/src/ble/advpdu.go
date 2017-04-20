b214890819489ee16622a6543e73777edec84295
Michał Derkacz <michal@Lnet.pl>
2017-04-20 18:55:59 +0200

    examples/core51822/ble-connect: Try do connectable example.

---
+++ egpath/src/ble/advpdu.go
@@ -0,0 +1,163 @@
+package ble
+
+// AdvPDU represents Advertising Channel PDU. It should be initialized before
+// use. AdvPDU can hold no more than 37 bytes of payload.
+type AdvPDU struct {
+	b []byte
+}
+
+// MakeAdvPDU returns ready to use AdvPDU. If b is nil then MakeAdvPDU allocates
+// 39 bytes that is enough to store any valid Advertising Channel PDU. Returned
+// variable refers to the same memory as b or to the allocated storage.
+func MakeAdvPDU(b []byte) AdvPDU {
+	if b == nil {
+		b = make([]byte, 39)
+	}
+	b[0] = 0
+	b[1] = 0
+	return AdvPDU{b}
+}
+
+// IsNil returns true in case of uninitialized pdu variable.
+func (pdu AdvPDU) IsNil() bool {
+	return pdu.b == nil
+}
+
+func (pdu AdvPDU) length() int {
+	return int(pdu.b[1])&0x3F + 2
+}
+
+func (pdu AdvPDU) setLength(n int) {
+	if uint(n) > 39 {
+		panic("ble: bad PDU length")
+	}
+	pdu.b[1] = byte(n - 2)
+}
+
+// Bytes returns underlying bytes of pdu.
+func (pdu AdvPDU) Bytes() []byte {
+	return pdu.b[:pdu.length()]
+}
+
+// AdvPDUType represents Advertising Channel PDU type.
+type AdvPDUType byte
+
+const (
+	AdvInd        AdvPDUType = 0
+	AdvDirectInd  AdvPDUType = 1
+	AdvNonconnInd AdvPDUType = 2
+	ScanReq       AdvPDUType = 3
+	ScanRsp       AdvPDUType = 4
+	ConnectReq    AdvPDUType = 5
+	AdvScanInd    AdvPDUType = 6
+)
+
+// Type returns pdu type from header.
+func (pdu AdvPDU) Type() AdvPDUType {
+	return AdvPDUType(pdu.b[0] & 0xf)
+}
+
+// SetType sets pdu type in header.
+func (pdu AdvPDU) SetType(typ AdvPDUType) {
+	pdu.b[0] = pdu.b[0]&^0xf | byte(typ)
+	pdu.Reset()
+}
+
+// TxAdd returns TxAdd field from header.
+func (pdu AdvPDU) TxAdd() bool {
+	return pdu.b[0]<<6&1 != 0
+}
+
+// SetTxAdd sets TxAdd field in header.
+func (pdu AdvPDU) SetTxAdd(rndaddr bool) {
+	if rndaddr {
+		pdu.b[0] |= 1 << 6
+	} else {
+		pdu.b[0] &^= 1 << 6
+	}
+}
+
+// Reset resets length of pdu.
+func (pdu AdvPDU) Reset() {
+	pdu.setLength(2)
+}
+
+func (pdu AdvPDU) AppendAddr(addr uint64) {
+	n := pdu.length()
+	pdu.setLength(n + 6)
+	pdu.b[n] = byte(addr)
+	pdu.b[n+1] = byte(addr >> 8)
+	pdu.b[n+2] = byte(addr >> 16)
+	pdu.b[n+3] = byte(addr >> 24)
+	pdu.b[n+4] = byte(addr >> 32)
+	pdu.b[n+5] = byte(addr >> 40)
+}
+
+// Flags
+const (
+	LimitedDisc   = 1 << 0 // LE Limited Discoverable mode.
+	GeneralDisc   = 1 << 1 // LE General Discoverable mode.
+	OnlyLE        = 1 << 2 // BR/EDR not supported.
+	LE_BREDR_Ctrl = 1 << 3 // Simultaneous LE and BR/EDR (controller).
+	LE_BREDR_Host = 1 << 4 // Simultaneous LE and BR/EDR (host).
+)
+
+type ADType byte
+
+const (
+	Flags ADType = 0x01 // Flags
+
+	MoreServices16 ADType = 0x02 // More 16-bit UUIDs available.
+	Services16     ADType = 0x03 // Complete list of 16-bit UUIDs available.
+	MoreServices32 ADType = 0x04 // More 32-bit UUIDs available.
+	Services32     ADType = 0x05 // Complete list of 32-bit UUIDs available.
+	MoreServices   ADType = 0x06 // More 128-bit UUIDs available.
+	Services       ADType = 0x07 // Complete list of 128-bit UUIDs available.
+
+	ShortLocalName ADType = 0x08 // Shortened local name.
+	LocalName      ADType = 0x09 // Complete local name.
+
+	TxPower ADType = 0x0A // Tx Power Level: -127 to +127 dBm.
+
+	ManufSpecData ADType = 0xFF // Manufacturer Specific Data.
+)
+
+func (pdu AdvPDU) AppendBytes(typ ADType, s ...byte) {
+	n := pdu.length()
+	pdu.setLength(n + 2 + len(s))
+	pdu.b[n] = byte(1 + len(s))
+	pdu.b[n+1] = byte(typ)
+	copy(pdu.b[n+2:], s)
+}
+
+func (pdu AdvPDU) AppendWords16(typ ADType, s ...uint16) {
+	n := pdu.length()
+	pdu.setLength(n + 2 + 2*len(s))
+	pdu.b[n] = byte(1 + 2*len(s))
+	pdu.b[n+1] = byte(typ)
+	for i, u := range s {
+		pdu.b[n+2+2*i] = byte(u)
+		pdu.b[n+3+2*i] = byte(u >> 8)
+	}
+}
+
+func (pdu AdvPDU) AppendWords32(typ ADType, s ...uint32) {
+	n := pdu.length()
+	pdu.setLength(n + 2 + 4*len(s))
+	pdu.b[n] = byte(1 + 4*len(s))
+	pdu.b[n+1] = byte(typ)
+	for i, u := range s {
+		pdu.b[n+2+4*i] = byte(u)
+		pdu.b[n+3+4*i] = byte(u >> 8)
+		pdu.b[n+4+4*i] = byte(u >> 16)
+		pdu.b[n+5+4*i] = byte(u >> 24)
+	}
+}
+
+func (pdu AdvPDU) AppendString(typ ADType, s string) {
+	n := pdu.length()
+	pdu.setLength(n + 2 + len(s))
+	pdu.b[n] = byte(1 + len(s))
+	pdu.b[n+1] = byte(typ)
+	copy(pdu.b[n+2:], s)
+}
