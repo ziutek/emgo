23bec39e46451562bf8bfab070aa9f5add29fef4
Michał Derkacz <michal@Lnet.pl>
2017-08-05 11:42:53 +0200

    Fix bug in EQUALI.

--- egroot/egc/internal/interface.h
+++ egroot/egc/internal/interface.h
@@ -1,17 +1,18 @@
 typedef union {
 	unsafe$Pointer ptr;
-	complex128     c128;
+	uint64         u128[2];
 	slice          sli;
 
 	// Additional fields, useful for debugging:
-	int8      i8;
-	int16     i16;
-	int32     i32;
-	int64     i64;
-	float32   f32;
-	float64   f64;
-	complex64 c64;
-	string    str;
+	int8       i8;
+	int16      i16;
+	int32      i32;
+	int64      i64;
+	float32    f32;
+	float64    f64;
+	complex64  c64;
+	complex128 c128;
+	string     str;
 } ival;
 
 typedef struct {
@@ -19,14 +20,23 @@ typedef struct {
 	const void *itab$;
 } interface;
 
-#define EQUALI(lhs, rhs) ({                           \
-	typeof(lhs) a = (lhs);                            \
-	typeof(rhs) b = (rhs);                            \
-	a.itab$ == b.itab$ && a.val$.c128 == b.val$.c128; \
+#define EQUALI(lhs, rhs) ({                    \
+	typeof(lhs) a = (lhs);                     \
+	typeof(rhs) b = (rhs);                     \
+	sizeof(a.val$.sli) > sizeof(a.val$.u128) ? \
+		a.itab$ == b.itab$ &&                  \
+		a.val$.sli.arr == b.val$.sli.arr &&    \
+		a.val$.sli.len == b.val$.sli.len &&    \
+		a.val$.sli.cap == b.val$.sli.cap       \
+	:                                          \
+		a.itab$ == b.itab$ &&                  \
+		a.val$.u128[0] == b.val$.u128[0] &&    \
+		a.val$.u128[1] == b.val$.u128[1]       \
+	;                                          \
 })
 
 
-#define _CAST(t, e) ({                               \
+#define _CAST(t, e) ({                              \
 	union {typeof(e) in; typeof(t) out;} cast = {}; \
 	cast.in = (e);                                  \
 	cast.out;                                       \
