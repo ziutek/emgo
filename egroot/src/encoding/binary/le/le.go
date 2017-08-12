d81185b963e5a4e6e4948df203a9a9766ddb3505
Michał Derkacz <michal@Lnet.pl>
2017-08-12 02:14:04 +0200

    New package: encoding/binary/le

---
+++ egroot/src/encoding/binary/le/le.go
@@ -0,0 +1,40 @@
+package le
+
+func Decode16(s []byte) uint16 {
+	return uint16(s[0]) | uint16(s[1])<<8
+}
+
+func Decode32(s []byte) uint32 {
+	return uint32(s[0]) | uint32(s[1])<<8 | uint32(s[2])<<16 | uint32(s[3])<<24
+}
+
+func Decode64(s []byte) uint64 {
+	l := uint(s[0]) | uint(s[1])<<8 | uint(s[2])<<16 | uint(s[3])<<24
+	h := uint(s[4]) | uint(s[5])<<8 | uint(s[6])<<16 | uint(s[7])<<24
+	return uint64(l) | uint64(h)<<32
+}
+
+func Encode16(s []byte, u uint16) {
+	s[0] = byte(u)
+	s[1] = byte(u >> 8)
+}
+
+func Encode32(s []byte, u uint32) {
+	s[0] = byte(u)
+	s[1] = byte(u >> 8)
+	s[2] = byte(u >> 16)
+	s[3] = byte(u >> 24)
+}
+
+func Encode64(s []byte, u uint64) {
+	l := uint(u)
+	s[0] = byte(l)
+	s[1] = byte(l >> 8)
+	s[2] = byte(l >> 16)
+	s[3] = byte(l >> 24)
+	h := uint(u >> 32)
+	s[4] = byte(h)
+	s[5] = byte(h >> 8)
+	s[6] = byte(h >> 16)
+	s[7] = byte(h >> 24)
+}
