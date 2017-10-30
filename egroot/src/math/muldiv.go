85a0ef30c784e15b8a301b869f1893f54a3b4257
Michał Derkacz <michal@Lnet.pl>
2017-10-30 20:12:18 +0100

    math: Add MulDivUp. Remove MulDiv32.

--- egroot/src/math/muldiv.go
+++ egroot/src/math/muldiv.go
@@ -1,9 +1,19 @@
 package math
 
-func Muldiv(x, m, d uint64) uint64 {
+// MulDiv returns uint64(uint128(x) * uint128(m) / uint128(d)).
+func MulDiv(x, m, d uint64) uint64 {
 	divx := x / d
 	modx := x - divx*d
 	divm := m / d
 	modm := m - divm*d
 	return divx*m + modx*divm + modx*modm/d
+}
+
+func MulDivUp(x, m, d uint64) uint64 {
+	o := d - 1
+	divx := (x + o) / d
+	modx := x - divx*d
+	divm := (m + o) / d
+	modm := m - divm*d
+	return divx*m + modx*divm + (modx*modm+o)/d
 }
--- egroot/src/math/muldiv32.go
+++
@@ -1,9 +0,0 @@
-package math
-
-func Muldiv32(x, m, d uint32) uint32 {
-	divx := x / d
-	modx := x - divx*d
-	divm := m / d
-	modm := m - divm*d
-	return divx*m + modx*divm + modx*modm/d
-}
