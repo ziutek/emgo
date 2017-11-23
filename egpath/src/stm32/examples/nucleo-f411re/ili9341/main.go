3e93e505f50a8b8189d3dbce51e0e933183a83f6
Michał Derkacz <michal@Lnet.pl>
2017-11-23 22:31:45 +0100

    New nucleo-f411re/ili9341 example.

---
+++ egpath/src/stm32/examples/nucleo-f411re/ili9341/main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"delay"
+	"fmt"
+	"rtos"
+
+	"display/ili9341"
+	"display/ili9341/ili9341test"
+
+	"stm32/ilidci"
+
+	"stm32/hal/dma"
+	"stm32/hal/gpio"
+	"stm32/hal/irq"
+	"stm32/hal/spi"
+	"stm32/hal/system"
+	"stm32/hal/system/timer/systick"
+)
+
+var (
+	lcdspi *spi.Driver
+	lcd    *ili9341.Display
+)
+
+func init() {
+	system.Setup96(8)
+	systick.Setup(2e6)
+
+	// GPIO
+
+	gpio.A.EnableClock(true)
+	spiport, sck, miso, mosi := gpio.A, gpio.Pin5, gpio.Pin6, gpio.Pin7
+	ilics := gpio.A.Pin(15)
+
+	gpio.B.EnableClock(true)
+	ilidc := gpio.B.Pin(7)
+
+	gpio.C.EnableClock(true)
+	//spiport, sck, miso, mosi := gpio.C, gpio.Pin10, gpio.Pin11, gpio.Pin12
+	ilireset := gpio.C.Pin(13) // Max output: 2 MHz, 3 mA.
+
+	// SPI
+
+	spiport.Setup(sck|mosi, &gpio.Config{Mode: gpio.Alt, Speed: gpio.High})
+	spiport.Setup(miso, &gpio.Config{Mode: gpio.AltIn})
+	spiport.SetAltFunc(sck|miso|mosi, gpio.SPI1)
+	d := dma.DMA2
+	d.EnableClock(true)
+	lcdspi = spi.NewDriver(spi.SPI1, d.Channel(2, 3), d.Channel(3, 3))
+	lcdspi.P.EnableClock(true)
+	lcdspi.P.SetConf(
+		spi.Master | spi.MSBF | spi.CPOL0 | spi.CPHA0 |
+			lcdspi.P.BR(48e6) | // 50 MHz max.
+			spi.SoftSS | spi.ISSHigh,
+	)
+	lcdspi.P.SetWordSize(8)
+	lcdspi.P.Enable()
+	rtos.IRQ(irq.SPI1).Enable()
+	rtos.IRQ(irq.DMA2_Stream2).Enable()
+	rtos.IRQ(irq.DMA2_Stream3).Enable()
+
+	// Controll
+
+	cfg := gpio.Config{Mode: gpio.Out, Speed: gpio.High}
+	ilics.Setup(&cfg)
+	ilics.Set()
+	ilidc.Setup(&cfg)
+	cfg.Speed = gpio.Low
+	ilireset.Setup(&cfg)
+	delay.Millisec(1) // Reset pulse.
+	ilireset.Set()
+	delay.Millisec(5) // Wait for reset.
+	ilics.Clear()
+
+	lcd = ili9341.NewDisplay(ilidci.NewDCI(lcdspi, ilidc), 240, 320)
+}
+
+func main() {
+	delay.Millisec(100)
+	spibus := lcdspi.P.Bus()
+	baudrate := lcdspi.P.Baudrate(lcdspi.P.Conf())
+	fmt.Printf(
+		"\nSPI on %s (%d MHz). SPI speed: %d bps.\n\n",
+		spibus, spibus.Clock()/1e6, baudrate,
+	)
+	ili9341test.Run(lcd, 10, true)
+}
+
+func lcdSPIISR() {
+	lcdspi.ISR()
+}
+
+func lcdRxDMAISR() {
+	lcdspi.DMAISR(lcdspi.RxDMA)
+}
+
+func lcdTxDMAISR() {
+	lcdspi.DMAISR(lcdspi.TxDMA)
+}
+
+//emgo:const
+//c:__attribute__((section(".ISRs")))
+var ISRs = [...]func(){
+	irq.SPI1:          lcdSPIISR,
+	irq.DMA2_Stream2: lcdRxDMAISR,
+	irq.DMA2_Stream3: lcdTxDMAISR,
+}
