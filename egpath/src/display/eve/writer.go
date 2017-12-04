60983684a2116caa55847399c2c2ab1d31e8a548
Michał Derkacz <michal@Lnet.pl>
2017-12-04 18:40:11 +0100

    display/eve: Fix bug in Writer.Write.

--- egpath/src/display/eve/writer.go
+++ egpath/src/display/eve/writer.go
@@ -19,6 +19,16 @@ func (w Writer) align32() {
 	d.buf = d.buf[:m]
 }
 
+func (w Writer) wr8(b byte) {
+	d := w.d
+	if len(d.buf) == cap(d.buf) {
+		d.flush()
+	}
+	n := len(d.buf)
+	d.buf = d.buf[:n+1]
+	d.buf[n] = b
+}
+
 func (w Writer) wr32(u uint32) {
 	d := w.d
 	if len(d.buf)+4 > cap(d.buf) {
@@ -37,14 +47,14 @@ func (w Writer) aw32(u uint32) {
 	w.wr32(u)
 }
 
-func (w Writer) Write32(s ...uint32) {
-	w.align32()
-	for _, u := range s {
-		w.wr32(u)
+func (w Writer) Write8(s ...byte) {
+	if len(s) == 1 {
+		w.wr8(s[0])
+		return
 	}
-}
-
-func (w Writer) Write(s []byte) (int, error) {
+	if len(s) == 0 {
+		return
+	}
 	d := w.d
 	if len(s) >= cap(d.buf) {
 		if len(d.buf) > 0 {
@@ -52,33 +62,58 @@ func (w Writer) Write(s []byte) (int, error) {
 		}
 		d.dci.Write(s)
 		d.n += len(s)
-	} else {
-		n := copy(d.buf[:cap(d.buf)], s)
-		d.buf = d.buf[:len(d.buf)+n]
-		if n < len(s) {
-			d.flush()
-			d.buf = d.buf[:len(s)-n]
-			copy(d.buf, s[n:])
-		}
+		return
 	}
-	// Write always succeeds. There is no case for infinite write transaction
-	// so Driver.Err can be called after all writes.
-	return len(s), nil
+	n := len(d.buf)
+	m := copy(d.buf[n:cap(d.buf)], s)
+	d.buf = d.buf[:n+m]
+	if m < len(s) {
+		s = s[m:]
+		d.flush()
+		d.buf = d.buf[:len(s)]
+		copy(d.buf, s)
+	}
 }
 
-func (w Writer) WriteString(s string) (int, error) {
+func (w Writer) Write32(s ...uint32) {
+	w.align32()
+	for _, u := range s {
+		w.wr32(u)
+	}
+}
+
+func (w Writer) WriteInt(s ...int) {
+	w.align32()
+	for _, i := range s {
+		w.wr32(uint32(i))
+	}
+}
+
+func (w Writer) Write(s []byte) (int, error) {
+	w.Write8(s...)
+	return len(s), nil
+	// BUG?: Write always succeeds. Rationale: there is no case for infinite
+	// write transaction so Driver.Err can be called after all writes.
+}
+
+func (w Writer) wrs(s string) {
 	d := w.d
-	n := len(s)
 	for len(s) != 0 {
 		if len(d.buf) == cap(d.buf) {
 			d.flush()
 		}
-		m := len(d.buf)
-		c := copy(d.buf[m:cap(d.buf)], s)
-		d.buf = d.buf[:m+c]
+		n := len(d.buf)
+		c := copy(d.buf[n:cap(d.buf)], s)
+		d.buf = d.buf[:n+c]
 		s = s[c:]
 	}
-	return n, nil
+}
+
+func (w Writer) WriteString(s string) (int, error) {
+	w.wrs(s)
+	return len(s), nil
+	// BUG?: WriteString always succeeds. Rationale: there is no case for
+	// infinite write transaction so Driver.Err can be called after all writes.
 }
 
 // Close closes the write transaction and returns number of bytes written.
