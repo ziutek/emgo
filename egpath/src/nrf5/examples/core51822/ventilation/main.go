1bbe8ea508a4743221d66abfb628864133625457
Michał Derkacz <michal@Lnet.pl>
2018-01-04 00:20:52 +0100

    examples/core51822/ventilation: Display test.

--- egpath/src/nrf5/examples/core51822/ventilation/main.go
+++ egpath/src/nrf5/examples/core51822/ventilation/main.go
@@ -1,3 +1,11 @@
+// This code drives ventilation unit controller. Ventilation unit consist of:
+// a counter-flow recuperator, two DC fans (EBM R1G225-AF33-12) and two air
+// filters.
+//
+// Controller produces two PWM signals to contoll both fans. The current fan
+// speed is mesured by counting pulses from speed output. The desired speed can
+// be set using rotary encoder. Controller has two 4-digit 7-segment displays,
+// that shows the current speed of both fans.
 package main
 
 import (
@@ -11,10 +19,23 @@ import (
 	"nrf5/hal/system/timer/rtcst"
 )
 
-// Two BW428G-E4 four digits, LED displays (common cathode).
+const (
+	A = iota
+	B
+	C
+	D
+	E
+	F
+	G
+	Q
+)
+
+// Two 4-digit 7-segment displays (BW428G-E4, common cathode).
 type Display struct {
-	dig [8]gpio.Pin // 0-3 top display, 4-7 bottom display.
-	seg [8]gpio.Pin // A B C D E F G :
+	dig    [8]gpio.Pins // 0-3 top display, 4-7 bottom display.
+	seg    [8]gpio.Pins // A B C D E F G :
+	digAll gpio.Pins
+	segAll gpio.Pins
 }
 
 var disp Display
@@ -27,79 +48,54 @@ func init() {
 	// Allocate pins (always do it in one place to avoid conflicts).
 
 	p0 := gpio.P0
-	disp.seg[5] = p0.Pin(0)  // F
-	disp.dig[5] = p0.Pin(1)  // Bottom 1
-	disp.dig[6] = p0.Pin(2)  // Bottom 2
-	disp.seg[3] = p0.Pin(3)  // D
-	disp.seg[4] = p0.Pin(9)  // E
-	disp.dig[7] = p0.Pin(11) // Bottom 3
-	disp.dig[2] = p0.Pin(15) // Top 2
-	disp.seg[6] = p0.Pin(17) // 6
-	disp.seg[1] = p0.Pin(22) // B
-	disp.seg[2] = p0.Pin(23) // C
-	disp.seg[7] = p0.Pin(21) // :
-	disp.dig[0] = p0.Pin(24) // Top 0
-	disp.dig[3] = p0.Pin(25) // Top 3
-	disp.seg[0] = p0.Pin(28) // A
-	disp.dig[1] = p0.Pin(29) // Top 1
-	disp.dig[4] = p0.Pin(30) // Bottom 0
+	disp.seg[F] = gpio.Pin0  // F
+	disp.dig[5] = gpio.Pin1  // Bottom 1
+	disp.dig[6] = gpio.Pin2  // Bottom 2
+	disp.seg[D] = gpio.Pin3  // D
+	disp.seg[E] = gpio.Pin9  // E
+	disp.dig[7] = gpio.Pin11 // Bottom 3
+	disp.dig[2] = gpio.Pin15 // Top 2
+	disp.seg[G] = gpio.Pin17 // G
+	disp.seg[B] = gpio.Pin22 // B
+	disp.seg[C] = gpio.Pin23 // C
+	disp.seg[Q] = gpio.Pin21 // :
+	disp.dig[0] = gpio.Pin24 // Top 0
+	disp.dig[3] = gpio.Pin25 // Top 3
+	disp.seg[A] = gpio.Pin28 // A
+	disp.dig[1] = gpio.Pin29 // Top 1
+	disp.dig[4] = gpio.Pin30 // Bottom 0
 
 	// Configure pins.
 
 	for _, pin := range disp.dig {
-		if !pin.IsValid() {
-			continue
-		}
-		// Drive digits with higd drive, open collector.
-		pin.Set()
-		pin.Setup(gpio.ModeOut | gpio.DriveH0D1)
+		disp.digAll |= pin
 	}
 	for _, pin := range disp.seg {
-		if !pin.IsValid() {
-			continue
-		}
-		pin.Setup(gpio.ModeOut | gpio.DriveD0H1)
+		disp.segAll |= pin
 	}
+	// Drive digits with higd drive, open drain (n-channel).
+	p0.SetPins(disp.digAll)
+	p0.Setup(disp.digAll, gpio.ModeOut|gpio.DriveH0D1)
+	// Drive segments with higd drive, open drain (p-channel).
+	p0.Setup(disp.segAll, gpio.ModeOut|gpio.DriveD0H1)
 }
 
 func wait() {
-	delay.Millisec(1000)
+	delay.Millisec(500)
 }
 
 func main() {
-	disp.seg[0].Set()
-	disp.seg[1].Set()
-	disp.seg[2].Set()
-	disp.seg[3].Set()
-	disp.seg[4].Set()
-	disp.seg[5].Set()
-	disp.seg[6].Set()
-	disp.seg[7].Set()
+	p0 := gpio.P0
 	for {
-		disp.dig[7].Set()
-		disp.dig[0].Clear()
-		wait()
-		disp.dig[0].Set()
-		disp.dig[1].Clear()
-		wait()
-		disp.dig[1].Set()
-		disp.dig[2].Clear()
-		wait()
-		disp.dig[2].Set()
-		disp.dig[3].Clear()
-		wait()
-		disp.dig[3].Set()
-		disp.dig[4].Clear()
-		wait()
-		disp.dig[4].Set()
-		disp.dig[5].Clear()
-		wait()
-		disp.dig[5].Set()
-		disp.dig[6].Clear()
-		wait()
-		disp.dig[6].Set()
-		disp.dig[7].Clear()
-		wait()
+		for _, dig := range disp.dig {
+			p0.SetPins(disp.digAll)
+			p0.ClearPins(dig)
+			for _, seg := range disp.seg {
+				p0.ClearPins(disp.segAll)
+				p0.SetPins(seg)
+				delay.Millisec(200)
+			}
+		}
 	}
 }
 
