cd2494df7e4468e3b146eecbb621af617eb27ac1
Michał Derkacz <michal@Lnet.pl>
2018-01-16 21:21:27 +0100

    nrf5/hal/ppi: Rename ChanGroup to Group.

--- egpath/src/nrf5/hal/ppi/changroup.go
+++ egpath/src/nrf5/hal/ppi/changroup.go
@@ -4,26 +4,26 @@ import (
 	"nrf5/hal/te"
 )
 
-// ChanGroup repersents PPI channel group. There are 6 channel groups numbered
-// from 0 to 5.
-type ChanGroup byte
+// Group repersents PPI channel group. There are 6 channel groups numbered from
+// 0 to 5.
+type Group byte
 
-// Channels returns channels that belongs to the group c.
-func (g ChanGroup) Channels() Channels {
+// Channels returns channels that belongs to the group g.
+func (g Group) Channels() Channels {
 	return Channels(r().chg[g].Load())
 }
 
-// SetChannels sets channels that belongs to the group c.
-func (g ChanGroup) SetChannels(c Channels) {
+// SetChannels sets channels that belongs to the group g.
+func (g Group) SetChannels(c Channels) {
 	r().chg[g].Store(uint32(c))
 }
 
 // EN returns task that can be used to enable channel group g.
-func (g ChanGroup) EN() *te.Task {
+func (g Group) EN() *te.Task {
 	return r().Task(int(g) * 2)
 }
 
 // DIS returns task that can be used to disable channel group g.
-func (g ChanGroup) DIS() *te.Task {
+func (g Group) DIS() *te.Task {
 	return r().Task(int(g)*2 + 1)
 }
