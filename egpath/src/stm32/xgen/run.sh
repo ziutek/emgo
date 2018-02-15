7423a8b13843fab15f19ea8cc3027db7b8ed57b0
Michał Derkacz <michal@Lnet.pl>
2018-02-15 23:29:04 +0100

    stm32: Generate raw HAL for f030x6.

--- egpath/src/stm32/xgen/run.sh
+++ egpath/src/stm32/xgen/run.sh
@@ -14,6 +14,7 @@ $ud STM32L1XX_MD   stm32l1xx.h |stm32xgen stm32/o/l1xx_md
 $ud STM32F10X_MD   stm32f10x.h |stm32xgen stm32/o/f10x_md
 $ud STM32F10X_HD   stm32f10x.h |stm32xgen stm32/o/f10x_hd
 
+stm32xgen stm32/o/f030x6 <stm32f0xx/stm32f030x6.h
 stm32xgen stm32/o/f303xe <stm32f3xx/stm32f303xe.h
 stm32xgen stm32/o/f746xx <stm32f7xx/stm32f746xx.h
 stm32xgen stm32/o/l476xx <stm32l4xx/stm32l476xx.h
