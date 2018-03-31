2131cdf644ae95aa26a200934dd7e031db029df7
Michał Derkacz <michal@Lnet.pl>
2018-03-31 11:06:50 +0200

    display/eve: Add package description.

--- egpath/src/display/eve/doc.go
+++ egpath/src/display/eve/doc.go
@@ -1,3 +1,4 @@
 // Package eve provides driver for FTDI Embedded Video Engine. It provides low
 // and high level interface. The high level interface handles both EVE (FT80x)
 // and EVE2 (FT81x) controllers in unified way.
+package eve
