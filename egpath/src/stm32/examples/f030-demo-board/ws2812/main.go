f87b16101224abab22d0c6e6922f092367c167a3
Michał Derkacz <michal@Lnet.pl>
2018-04-16 01:06:15 +0200

    stm32/examples/f030-demo-board/ws2812: New example.

---
+++ egpath/src/stm32/examples/f030-demo-board/ws2812/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"delay"
+	"math/rand"
+	"rtos"
+
+	"ws281x"
+
+	"stm32/hal/dma"
+	"stm32/hal/gpio"
+	"stm32/hal/irq"
+	"stm32/hal/system"
+	"stm32/hal/system/timer/systick"
+	"stm32/hal/usart"
+)
+
+var tts *usart.Driver
+
+func init() {
+	system.SetupPLL(8, 1, 48/8)
+	systick.Setup(2e6)
+
+	gpio.A.EnableClock(true)
+	tx := gpio.A.Pin(9)
+
+	tx.Setup(&gpio.Config{Mode: gpio.Alt})
+	tx.SetAltFunc(gpio.USART1_AF1)
+	d := dma.DMA1
+	d.EnableClock(true)
+
+	// WS2812 bit should take 1390 ns -> 463 ns for UART bit -> 2158273 bit/s.
+
+	tts = usart.NewDriver(usart.USART1, d.Channel(2, 0), nil, nil)
+	tts.Periph().EnableClock(true)
+	tts.Periph().SetBaudRate(3000000000 / 1390)
+	tts.Periph().SetConf2(usart.TxInv) // STM32F0 need no external inverter.
+	tts.Periph().Enable()
+	tts.EnableTx()
+
+	rtos.IRQ(irq.USART1).Enable()
+	rtos.IRQ(irq.DMA1_Channel2_3).Enable()
+}
+
+func main() {
+	var rnd rand.XorShift64
+	rnd.Seed(1)
+	fb := ws281x.MakeFBU(3)
+	for {
+		fb.At(0).EncodeGRB(ws281x.Color(rnd.Uint32()).Gamma())
+		fb.At(1).EncodeGRB(ws281x.Color(rnd.Uint32()).Gamma())
+		fb.At(2).EncodeGRB(ws281x.Color(rnd.Uint32()).Gamma())
+		tts.Write(fb.Bytes())
+		delay.Millisec(1e3)
+	}
+}
+
+func ttsISR() {
+	tts.ISR()
+}
+
+func ttsDMAISR() {
+	tts.TxDMAISR()
+}
+
+//c:__attribute__((section(".ISRs")))
+var ISRs = [...]func(){
+	irq.USART1:          ttsISR,
+	irq.DMA1_Channel2_3: ttsDMAISR,
+}
