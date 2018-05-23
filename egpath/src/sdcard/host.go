4be6d0a90e4bb2f7335d013efa2ae9b549a03a7e
Michał Derkacz <michal@Lnet.pl>
2018-05-24 00:22:42 +0200

    sdcard: Two new methods in Host interface.

--- egpath/src/sdcard/host.go
+++ egpath/src/sdcard/host.go
@@ -7,14 +7,19 @@ import (
 var ErrTimeout = errors.New("sdio: timeout")
 
 type Host interface {
-	// Cmd sends a command to the card and receives its response, if any. Short
+	// SetFreq sets the CLK/SCLK clock frequency to freqHz. Host can implement
+	// disabling clock output if the bus is idle and pwrsave is set to true.
+	SetFreq(freqHz int, pwrsave bool)
+
+	// SetBusWidth allow to change the the host data bus width.
+	SetBusWidth(width int)
+
+	// Cmd sends the cmd to the card and receives its response, if any. Short
 	// response is returned in r[0]. Long is returned in r[0:3] (r[0] contains
 	// the least significant bits, r[3] contains the most significant bits).
 	Cmd(cmd Command, arg uint32) (r Response)
 
-	// Err returns and clears internal error status. The internal error status,
-	// if set,  prevents any subsequent operations on the card. the ErrTimeout
-	// should be returned if the internal error represents the lack of reponse
-	// from the card.
+	// Err returns and clears the host internal error. The internal error, if
+	// not nil, prevents any subsequent operations on the card.
 	Err(clear bool) error
 }
