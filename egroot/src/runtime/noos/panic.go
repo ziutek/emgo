705ae943b0aaa7ca2af4cd36a8f95eb91aacddb7
Michał Derkacz <michal@Lnet.pl>
2018-06-01 11:39:41 +0200

    runtime/noos: Revert to print panic message to SWO.

--- egroot/src/runtime/noos/panic.go
+++ egroot/src/runtime/noos/panic.go
@@ -2,33 +2,30 @@
 package noos
 
 import (
-	//"arch/cortexm/debug/itm"
+	"arch/cortexm/debug/itm"
 )
 
-/*
 const dbg = itm.Port(0)
 
 type stringer interface {
 	String() string
 }
-*/
 
 func panic_(i interface{}) {
-	/*
-		var s string
-		switch v := i.(type) {
-		case error:
-			s = v.Error()
-		case stringer:
-			s = v.String()
-		case string:
-			s = v
-		default:
-		}
-		dbg.WriteString("panic: ")
-		dbg.WriteString(s)
-		dbg.WriteByte('\n')
-	*/
+	var s string
+	switch v := i.(type) {
+	case error:
+		s = v.Error()
+	case stringer:
+		s = v.String()
+	case string:
+		s = v
+	default:
+		s = "<no text descr>"
+	}
+	dbg.WriteString("\npanic: ")
+	dbg.WriteString(s)
+	dbg.WriteByte('\n')
 	for {
 	}
 }
