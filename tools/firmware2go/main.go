8e3a24af824918e4ec7231962aee5e5c8490a523
Michał Derkacz <michal@Lnet.pl>
2018-09-28 01:44:49 +0200

    tools/firmware2go: Emit bytes or string.

--- tools/firmware2go/main.go
+++ tools/firmware2go/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
+	"io/ioutil"
 	"os"
 )
 
@@ -11,58 +11,46 @@ func checkErr(err error) {
 	if err == nil {
 		return
 	}
-	fmt.Fprintln(os.Stderr, err)
+	os.Stderr.WriteString(err.Error())
 	os.Exit(1)
 }
 
 func main() {
-	if len(os.Args) != 4 || (os.Args[1] != "le" && os.Args[1] != "be") {
-		fmt.Fprintf(
-			os.Stderr, "Usage: firmware2go le|be INPUT.bin OUTPUT.go\n",
-		)
+	if len(os.Args) != 3 || os.Args[1] != "bytes" && os.Args[1] != "string" {
+		os.Stderr.WriteString("Usage: firmware2go {bytes|string} BINARY_FILE\n")
 		os.Exit(1)
 	}
-	le := os.Args[1] != "le"
-	f, err := os.Open(os.Args[2])
+	data, err := ioutil.ReadFile(os.Args[2])
 	checkErr(err)
-	inp := bufio.NewReader(f)
-	f, err = os.Create(os.Args[3])
+	w := bufio.NewWriter(os.Stdout)
+	_, err = w.WriteString("package main\n\n")
 	checkErr(err)
-	defer f.Close()
-	out := bufio.NewWriter(f)
-	_, err = out.WriteString(
-		"package main\n" + "//emgo:const\n" + "var firmware = [...]uint64{\n",
-	)
-	checkErr(err)
-	var buf [8]byte
-	for i := 0; ; i++ {
-		n, err := io.ReadFull(inp, buf[:])
-		if err != nil && err != io.ErrUnexpectedEOF {
-			if err == io.EOF {
-				if i%3 != 0 {
-					checkErr(out.WriteByte('\n'))
-				}
-				break
+	if os.Args[1] == "bytes" {
+		_, err = w.WriteString("//emgo:const\nvar firmware = [...]byte{")
+		checkErr(err)
+		for i, b := range data {
+			if i%15 == 0 {
+				_, err = w.WriteString("\n\t")
+				checkErr(err)
 			}
+			_, err = fmt.Fprintf(w, " %d,", b)
 			checkErr(err)
 		}
-		var v uint64
-		if le {
-			for k := 0; k < n; k++ {
-				v |= uint64(buf[k]) << uint(8*k)
-			}
-		} else {
-			for k := 0; k < n; k++ {
-				v |= uint64(buf[k]) << uint(56-8*k)
-			}
-		}
-		_, err = fmt.Fprintf(out, " 0x%016X,", v)
+		_, err = w.WriteString("\n}\n")
 		checkErr(err)
-		if i%3 == 2 {
-			checkErr(out.WriteByte('\n'))
+	} else {
+		_, err = w.WriteString("const firmware = \"")
+		checkErr(err)
+		for i, b := range data {
+			if i%18 == 0 {
+				_, err = w.WriteString("\" +\n\t\"")
+				checkErr(err)
+			}
+			_, err = fmt.Fprintf(w, "\\x%02X", b)
+			checkErr(err)
 		}
+		_, err = w.WriteString("\"\n")
+		checkErr(err)
 	}
-	_, err = out.WriteString("}\n")
-	checkErr(err)
-	checkErr(out.Flush())
+	checkErr(w.Flush())
 }
